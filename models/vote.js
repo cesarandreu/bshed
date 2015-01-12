3b522b907969f683aa3a98ffb5df4b9805a101a9
Cesar Andreu <cesarandreu@gmail.com>
2015-01-11 22:01:01 -0800

    add asObject to Vote model

--- models/vote.js
+++ models/vote.js
@@ -27,6 +27,11 @@ module.exports = function (sequelize, DataTypes) {
         models.Vote.belongsTo(models.Bike);
         models.Vote.belongsTo(models.Bikeshed);
         models.Vote.belongsTo(models.User);
+      },
+      asObject: function asObject (votes) {
+        var result = {};
+        votes.forEach(vote => result[vote.BikeId] = {value: vote.value});
+        return result;
       }
     }
   });
