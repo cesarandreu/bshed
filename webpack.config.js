ad943899eea28deeb7e7544d140a4469d2bc8ec7
Cesar Andreu <cesarandreu@gmail.com>
2015-01-13 20:47:17 -0800

    add more webpack loaders, fix asset extraction

--- webpack.config.js
+++ webpack.config.js
@@ -16,14 +16,15 @@ module.exports = {
   },
   module: {
     loaders: [
-      {test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-      {test: /\.(jsx|js)$/, exclude: /node_modules/, loader: '6to5-loader?runtime=true'}
+      {test: /\.(jsx|js)$/, exclude: /node_modules/, loader: '6to5-loader?runtime=true'},
+      {test: /\.less$/, loader: ExtractTextPlugin.extract('css-loader!less-loader')},
+      {test: /\.eot$/, loader: 'url-loader'}
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
-    new ExtractTextPlugin('styles.[hash].css', {allChunks: true})
+    new ExtractTextPlugin('styles.[hash].css')
   ]
 };
