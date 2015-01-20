b9ab93432a7bbc78359e8698f94870ae6217f2a5
Cesar Andreu <cesarandreu@gmail.com>
2015-01-19 20:04:28 -0800

    add Icon

---
+++ client/components/Icon.jsx
@@ -0,0 +1,24 @@
+'use strict';
+
+var React = require('react/addons'),
+  cx = React.addons.classSet,
+  _ = require('lodash');
+
+var Icon = React.createClass({
+  propTypes: {
+    icon: React.PropTypes.string.isRequired
+  },
+
+  render: function () {
+    var {className, icon, ...other} = this.props;
+    var classes = cx({
+      ['icon ' + icon]: true,
+      [className]: className,
+      md: _.contains(icon, 'md')
+    });
+
+    return <span {...other} className={classes}></span>;
+  }
+});
+
+module.exports = Icon;
