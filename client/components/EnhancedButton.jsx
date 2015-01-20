2a38ab343908e01a8721fc3eb34bbc49dce3420c
Cesar Andreu <cesarandreu@gmail.com>
2015-01-19 20:04:12 -0800

    add EnhancedButton

---
+++ client/components/EnhancedButton.jsx
@@ -0,0 +1,49 @@
+'use strict';
+
+var React = require('react/addons'),
+  cx = React.addons.classSet;
+
+var EnhancedButton = React.createClass({
+  propTypes: {
+    className: React.PropTypes.string,
+    disabled: React.PropTypes.bool,
+    link: React.PropTypes.bool,
+    onTouchTap: React.PropTypes.func
+  },
+
+  render: function () {
+    var {className, link, disabled, ...other} = this.props,
+    classes = cx({
+      'enhanced-button': true,
+      [className]: className,
+      disabled, link
+    });
+
+    var props = {
+      disabled,
+      className: classes,
+      onTouchTap: this._onTouchTap
+    };
+
+    var enhancedButton;
+    if (disabled && link) {
+      enhancedButton = (
+        <span {...other} className={classes} disabled={disabled}>
+          {this.props.children}
+        </span>
+      );
+    } else if (link) {
+      enhancedButton = <a {...other} {...props}>{this.props.children}</a>;
+    } else {
+      enhancedButton = <button {...other} {...props}>{this.props.children}</button>;
+    }
+
+    return enhancedButton;
+  },
+
+  _onTouchTap: function (e) {
+    if (this.props.onTouchTap) this.props.onTouchTap(e);
+  }
+});
+
+module.exports = EnhancedButton;
