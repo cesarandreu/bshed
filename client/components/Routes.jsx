46f7a2e1ab5938ea8b0326da636fc18fe4aa6b2f
Cesar Andreu <cesarandreu@gmail.com>
2015-02-05 21:17:02 -0800

    add bikeshed routes

--- client/components/Routes.jsx
+++ client/components/Routes.jsx
@@ -5,6 +5,7 @@ var routes = (
   <Route name='bshed' path='/' handler={require('./layout/Application.jsx')}>
     <DefaultRoute name='home' handler={require('./home/Home.jsx')}/>
     <Route name='about' handler={require('./about/About.jsx')}></Route>
+    {require('./bikesheds/routes.jsx')}
   </Route>
 );
 
