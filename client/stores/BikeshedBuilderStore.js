0819817a4d21f1d798e5ac4ec9cd5a33299e1389
Cesar Andreu <cesarandreu@gmail.com>
2015-02-22 22:25:53 -0800

    refactor BikeshedBuilderStore

--- client/stores/BikeshedBuilderStore.js
+++ client/stores/BikeshedBuilderStore.js
@@ -1,39 +1,51 @@
 var createStore = require('fluxible/utils/createStore'),
   _ = require('lodash');
 
+class Bike {
+  constructor (file) {
+    this.file = file;
+    this.name = file.name;
+    this.url = URL.createObjectURL(file);
+  }
+
+  destroy () {
+    this.url = URL.revokeObjectURL(this.url);
+  }
+}
+
 var BikeshedBuilderStore = createStore({
   storeName: 'BikeshedBuilderStore',
   handlers: {
-    'ADD_FILES_TO_BIKESHED': 'addFiles',
+    'ADD_BIKES_TO_BIKESHED_BUILDER': 'addBikes',
+    'REMOVE_BIKE_FROM_BIKESHED_BUILDER': 'removeBike'
   },
+
   initialize: function () {
     Object.assign(this, {
-      bikes: [],
-      bikesIndex: {}
+      index: {},
+      bikes: []
     });
   },
 
+  Bike: Bike,
+
   reindex: function () {
-    this.bikesIndex = _.indexBy(this.bikes, 'name');
+    this.index = _.indexBy(this.bikes, 'name');
   },
 
-  addFiles: function (files) {
-    files
-      .filter(f => !this.bikesIndex[f.name])
-      .forEach(f => {
-        this.bikes.push({
-          file: f,
-          name: f.name,
-          url: URL.createObjectURL(f)
-        });
-      });
+  addBikes: function (files) {
+    var bikes = files
+      .filter(file => !this.index[file.name])
+      .map(file => new Bike(file));
+
+    this.bikes = this.bikes.concat(bikes);
     this.reindex();
     this.emitChange();
   },
 
   removeBike: function (idx) {
-    URL.revokeObjectURL(this.bikes[idx].url);
-    this.bikes.splice(idx, 1);
+    var bike = this.bikes.splice(idx, 1);
+    bike.destroy();
     this.reindex();
     this.emitChange();
   },
@@ -43,8 +55,8 @@ var BikeshedBuilderStore = createStore({
   },
 
   getState: function () {
-    var {bikes} = this;
-    return {bikes}
+    var {bikes, index} = this;
+    return {bikes, index}
   },
   dehydrate: function () {
     return this.getState();
