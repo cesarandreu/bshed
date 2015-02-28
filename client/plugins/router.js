721e79228e8ee9e7bbc36036b630f42b714bcf7b
Cesar Andreu <cesarandreu@gmail.com>
2015-02-27 22:18:45 -0800

    refactor router plugin for readability

--- client/plugins/router.js
+++ client/plugins/router.js
@@ -3,20 +3,27 @@ var Router = require('react-router')
 module.exports = function RouterPlugin ({location}={}) {
   return {
     name: 'RouterPlugin',
-    plugContext (contextOptions={}) {
-      var router = Router.create({
-        location: contextOptions.url || contextOptions.location || location,
-        routes: contextOptions.app.getAppComponent(),
-      })
-      return {
-        plugActionContext (actionContext) {
-          actionContext.router = router
-        },
-        plugComponentContext (componentContext) {
-          componentContext.transitionTo = router.transitionTo.bind(router)
-          componentContext.router = router
-        }
-      }
+    plugContext
+  }
+
+  function plugContext (contextOptions) {
+    var router = Router.create({
+      location: contextOptions.url || contextOptions.location || location,
+      routes: contextOptions.app.getAppComponent()
+    })
+
+    return {
+      plugActionContext,
+      plugComponentContext
+    }
+
+    function plugActionContext (actionContext) {
+      actionContext.router = router
+    }
+
+    function plugComponentContext (componentContext) {
+      componentContext.transitionTo = router.transitionTo.bind(router)
+      componentContext.router = router
     }
   }
 }
