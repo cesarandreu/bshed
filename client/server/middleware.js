435ab799ffa307d6ee2c82a650fcb64b8d417ebb
Cesar Andreu <cesarandreu@gmail.com>
2015-02-27 22:26:27 -0800

    remove eslint warning

--- client/server/middleware.js
+++ client/server/middleware.js
@@ -2,22 +2,18 @@ var assert = require('assert'),
   request = require('../utils/request.server.js'),
   log = require('debug')('bshed:client:middleware')
 
-module.exports = function (opts={}) {
-  assert(opts.rendererPath)
-  assert(opts.assets)
-
-  var {rendererPath} = opts,
-    assets = opts.assets()
+module.exports = function ({rendererPath, assets}={}) {
+  assert(rendererPath && assets)
+  assets = assets()
   return function* client () {
-    var {url, csrf} = this
     try {
       log(`using renderer from ${rendererPath}`)
       var renderer = require(rendererPath)
       var {body, type, status} = yield renderer({
-        url, assets,
+        assets, url: this.url,
         request: request(this.app.server, {
           'cookie': this.get('cookie'), // send cookie header
-          'x-csrf-token': csrf // send csrf header
+          'x-csrf-token': this.csrf // send csrf header
         })
       })
       Object.assign(this, {status, body, type})
