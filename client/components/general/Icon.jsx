c2086e3d17217dfd68135f8f230e27d0026b1c5c
Cesar Andreu <cesarandreu@gmail.com>
2015-03-01 23:30:51 -0800

    refactor Icon, move styles

--- client/components/general/Icon.jsx
+++ client/components/general/Icon.jsx
@@ -1,22 +1,16 @@
-var React = require('react/addons'),
-  cx = React.addons.classSet,
-  _ = require('lodash');
+var React = require('react'),
+  classnames = require('classnames'),
+  _ = require('lodash')
 
 var Icon = React.createClass({
   propTypes: {
     icon: React.PropTypes.string.isRequired
   },
-
   render: function () {
-    var {className, icon, ...other} = this.props;
-    var classes = cx({
-      ['icon ' + icon]: true,
-      [className]: className,
-      md: _.contains(icon, 'md')
-    });
-
-    return <span {...other} className={classes}></span>;
+    var {className, icon, ...other} = this.props
+    var classes = classnames('icon', icon, className, {md: _.contains(icon, 'md')})
+    return <span {...other} className={classes}></span>
   }
-});
+})
 
-module.exports = Icon;
+module.exports = Icon
