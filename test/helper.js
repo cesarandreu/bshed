7febf46b7689adaebc280a27a6848cfb820194b8
Cesar Andreu <cesarandreu@gmail.com>
2015-03-06 20:59:40 -0800

    semicolons, es6 niceties

--- test/helper.js
+++ test/helper.js
@@ -1,8 +1,6 @@
-'use strict';
+'use strict'
 
-var chai = require('chai');
-
-global.expect = chai.expect;
+global.expect = require('chai').expect
 
 var path = require('path'),
   base = path.resolve(__dirname, '..'),
@@ -10,22 +8,22 @@ var path = require('path'),
   config = require(path.join(base, 'config')),
   models = require(path.join(base, 'models')),
   server = require(path.join(base, 'server')),
-  buildHeaders = require('./buildHeaders');
+  buildHeaders = require('./buildHeaders')
 
 // initialize stuff
 buildHeaders = buildHeaders({
   key: config.server.middleware.session.key,
   secret: config.secret
-});
+})
 models = models({
   database: config.database
-});
+})
 
 module.exports = {
-  base: base,
-  config: config,
-  models: models,
-  server: server,
-  fixtures: fixtures,
-  buildHeaders: buildHeaders
-};
+  base,
+  config,
+  models,
+  server,
+  fixtures,
+  buildHeaders
+}
