d24e1d2bdb6a9775cc61f267b8a3f24c449348fc
Cesar Andreu <cesarandreu@gmail.com>
2015-03-06 21:53:39 -0800

    semicolons, nicer assert

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
 var debug = require('debug')('bshed:api'),
   compose = require('koa-compose'),
   mount = require('koa-mount'),
   assert = require('assert'),
   qs = require('koa-qs'),
-  koa = require('koa');
+  koa = require('koa')
 
 var controllers = require('./controllers'),
-  helpers = require('./helpers');
+  helpers = require('./helpers')
 
 /**
  * API loader
@@ -17,29 +17,27 @@ var controllers = require('./controllers'),
  * returns api application
  */
 module.exports = function apiLoader ({s3, config, models}={}) {
-  assert(s3);
-  assert(config);
-  assert(models);
-  debug('loader:start');
-  var {name, env, secret, endpoint} = config;
-  var api = qs(koa());
+  assert(s3 && config && models)
+  debug('loader:start')
+  var {name, env, secret, endpoint} = config
+  var api = qs(koa())
   Object.assign(api, {
     name, env, secret, config, models, s3, controllers, helpers
-  });
+  })
   var middleware = compose([
     addToContext({models, s3, helpers}),
     controllers(helpers)
-  ]);
+  ])
 
-  api.use(mount(endpoint, middleware));
-  debug('loader:end');
-  return api;
-};
+  api.use(mount(endpoint, middleware))
+  debug('loader:end')
+  return api
+}
 
 // middleware~
 function addToContext (opts={}) {
   return function* addToContextMiddleware (next) {
-    Object.assign(this, opts);
-    yield next;
-  };
+    Object.assign(this, opts)
+    yield next
+  }
 }
