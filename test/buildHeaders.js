f7dfbb31b25350646934fb236714a6311b0a3d15
Cesar Andreu <cesarandreu@gmail.com>
2015-03-06 20:56:57 -0800

    use more es6 features

--- test/buildHeaders.js
+++ test/buildHeaders.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 // REFERENCES
 // https://github.com/pillarjs/cookies/blob/master/lib/cookies.js
@@ -8,18 +8,16 @@ var Cookie = require('cookies').Cookie,
   Keygrip = require('keygrip'),
   assert = require('assert'),
   csrf = require('csrf'),
-  _ = require('lodash');
+  _ = require('lodash')
 
 // secret - array of secrets for Keygrip
 // key - session cookie name
-module.exports = function buildHeadersHelper (opts) {
-  assert(opts);
-  assert(opts.secret);
-  assert(opts.key);
+module.exports = function buildHeadersHelper ({secret, key}={}) {
+  assert(secret && key, 'secret and key are needed')
 
   var maxAge = 24 * 60 * 60 * 1000,
-    keys = new Keygrip(opts.secret),
-    tokens = csrf();
+    keys = new Keygrip(secret),
+    tokens = csrf()
 
   return function buildHeaders (session) {
     // _expire and _maxAge are used by koa-session
@@ -27,21 +25,21 @@ module.exports = function buildHeadersHelper (opts) {
       _expire: maxAge + Date.now(),
       _maxAge: maxAge,
       secret: tokens.secretSync()
-    }, session);
+    }, session)
 
     var value = (new Buffer(JSON.stringify(session))).toString('base64'),
-      cookie = new Cookie(opts.key, value, {maxAge: maxAge}),
+      cookie = new Cookie(key, value, {maxAge}),
       headers = {
         'x-csrf-token': tokens.create(session.secret),
-        'cookie': cookie.toHeader() + ';'
-      };
+        'cookie': `${cookie.toHeader()};`
+      }
 
     // signed cookie
-    cookie.value = keys.sign(cookie.toString());
-    cookie.name += '.sig';
-    headers.cookie += cookie.toHeader() + ';';
+    cookie.value = keys.sign(cookie.toString())
+    cookie.name += '.sig'
+    headers.cookie += `${cookie.toHeader()};`
 
-    return headers;
-  };
+    return headers
+  }
 
-};
+}
