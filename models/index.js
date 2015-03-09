0de1332b6d8d408e90d9fb24e731c9538af81e2e
Cesar Andreu <cesarandreu@gmail.com>
2015-03-08 18:05:33 -0700

    remove useless space

--- models/index.js
+++ models/index.js
@@ -23,7 +23,7 @@ module.exports = function modelsLoader ({database}={}) {
 
   // Load models
   fs.readdirSync(__dirname)
-  .filter( file => file.indexOf('.') !== 0 && file !== 'index.js')
+  .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
   .forEach(file => {
     var modelPath = path.join(__dirname, file)
     var model = sequelize.import(modelPath)
