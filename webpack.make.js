361beb100d0b47f579989f1cbddf8f01fad7f33e
Cesar Andreu <cesarandreu@gmail.com>
2015-03-12 00:38:27 -0700

    add render config to webpack config maker

--- webpack.make.js
+++ webpack.make.js
@@ -8,15 +8,21 @@ module.exports = function buildWebpackConfig (options) {
   options = options || {}
 
   var ENV = options.ENV || process.env.NODE_ENV || 'development',
-    PRODUCTION = options.PRODUCTION || ENV === 'production'
+    DEVELOPMENT = options.DEVELOPMENT || ENV === 'development',
+    PRODUCTION = options.PRODUCTION || ENV === 'production',
+    SERVER = options.SERVER, DEBUG = options.DEBUG
 
   // shared values
   var publicPath = PRODUCTION ? '/assets/' : 'http://localhost:8080/assets/',
-    outputPath = path.join(__dirname, 'public', 'assets')
+    outputPath = path.join(__dirname, 'public', SERVER ? 'renderer' : 'assets')
+
+  var excludeFromStats = [
+    /node_modules[\\\/]react(-router)?[\\\/]/
+  ]
 
   // base
   var config = {
-    target: 'web',
+    debug: DEBUG,
     externals: [],
     module: {
       noParse: [/react-with-addons/],
@@ -35,37 +41,64 @@ module.exports = function buildWebpackConfig (options) {
       extensions: ['', '.js', '.jsx']
     },
     plugins: [
-      StatsPlugin(publicPath, outputPath),
-      new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(ENV)}),
-      new webpack.PrefetchPlugin('react')
-    ]
+      new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(ENV)})
+    ],
+    devServer: {
+      stats: {
+        exclude: excludeFromStats,
+        cached: false
+      }
+    }
   }
+
+  // target
+  config.target = SERVER ? 'node' : 'web'
 
   // Source maps
   config.devtool = PRODUCTION ? 'source-map' : 'eval-source-map'
 
   // entry
   config.entry = {
-    scripts: './client/index.js'
+    scripts: SERVER ? './client/server' : './client'
+  }
+
+  // externals
+  if (SERVER) {
+    // Every non-relative module is external
+    config.externals.push(/^[a-z\-0-9]+$/)
   }
 
   // output
+  var filename = SERVER ? 'index.js' : '[name].js',
+    chunkFilename = DEVELOPMENT && SERVER ? '[id].js' : '[name].js'
+
+  if (PRODUCTION && !SERVER) {
+    filename += '?[chunkhash]'
+    chunkFilename += '?[chunkhash]'
+  }
+
   config.output = {
     path: outputPath,
-    pathinfo: !PRODUCTION,
+    pathinfo: DEBUG,
     publicPath: publicPath,
-    filename: PRODUCTION ? 'scripts.[hash].js' : 'scripts.dev.js'
+    filename: filename,
+    chunkFilename: chunkFilename,
+    sourceMapFilename: 'debugging/[file].map',
+    libraryTarget: SERVER ? 'commonjs2' : void 0
   }
 
   // loaders
   var babelLoader = 'babel?experimental&optional=runtime'
+
+  // Webpack fails at parsing generator functions :(
+  // babelLoader += SERVER ? '&blacklist=regenerator' : ''
   config.module.loaders.push({
     test: /\.js$/,
     loader: babelLoader,
     exclude: /.*node_modules.*/
   })
 
-  var hotLoader = PRODUCTION ? '' : 'react-hot!'
+  var hotLoader = PRODUCTION || SERVER ? '' : 'react-hot!'
   config.module.loaders.push({
     test: /\.jsx$/,
     loader: hotLoader + babelLoader,
@@ -79,17 +112,22 @@ module.exports = function buildWebpackConfig (options) {
   })
 
   // plugins
-  if (PRODUCTION) {
-    // var cssName = PRODUCTION ? 'scripts.[hash].css' : 'scripts.dev.css'
-    config.plugins.push(new ExtractTextPlugin('scripts.[hash].css'))
+  if (!SERVER) {
+    config.plugins.push(new webpack.PrefetchPlugin('react'))
+    config.plugins.push(StatsPlugin())
+  }
 
+  if (PRODUCTION || SERVER) {
     // Ensures requires for 'react' and 'react/addons' normalize to the same path
     var reactAddonsPath = require.resolve('react/dist/react-with-addons'),
       reactRegex = /^react(\/addons)?$/
     config.plugins.push(new webpack.NormalModuleReplacementPlugin(reactRegex, reactAddonsPath))
+  }
 
-    // Minifify, dedupe
+  if (PRODUCTION && !SERVER) {
+    // Minifify, dedupe, extract css
     config.plugins.push(
+      new ExtractTextPlugin('[name].css?[contenthash]'),
       new webpack.optimize.UglifyJsPlugin(),
       new webpack.optimize.DedupePlugin(),
       new webpack.NoErrorsPlugin()
@@ -97,17 +135,20 @@ module.exports = function buildWebpackConfig (options) {
   }
 
   return config
-}
 
-function StatsPlugin (publicPath, outputPath) {
-  return function () {
-    this.plugin('done', saveStats)
-  }
+  function StatsPlugin () {
+    return function () {
+      this.plugin('done', saveStats)
+    }
 
-  function saveStats (stats) {
-    var jsonStats = stats.toJson()
-    jsonStats.publicPath = publicPath
-    mkdirp.sync(outputPath)
-    fs.writeFileSync(path.join(outputPath, 'stats.json'), JSON.stringify(jsonStats))
+    function saveStats (stats) {
+      var jsonStats = stats.toJson({
+        exclude: excludeFromStats,
+        chunkModules: true
+      })
+      jsonStats.publicPath = publicPath
+      mkdirp.sync(outputPath)
+      fs.writeFileSync(path.join(outputPath, 'stats.json'), JSON.stringify(jsonStats))
+    }
   }
 }
