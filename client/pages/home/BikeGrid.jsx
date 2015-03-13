32a4e5dbac9c2dfd913a3a69c613f1b2314d2b8a
Cesar Andreu <cesarandreu@gmail.com>
2015-03-12 22:02:43 -0700

    refactoring grid styles to be more card-y

--- client/pages/home/BikeGrid.jsx
+++ client/pages/home/BikeGrid.jsx
@@ -9,13 +9,12 @@ var BikeGrid = React.createClass({
     var items = bikes.map((bike, key) => {
       return (
         <div className='bike-item' key={key}>
+          {/*<IconButton className='bike-item-action' icon='md-info'/>*/}
           <div className='bike-item-image'>
             <img src={bike.url}/>
           </div>
-          <div className='bike-item-info'>
+          <div className='bike-item-metadata'>
             <div className='bike-item-name'>{bike.name}</div>
-            <IconButton className='bike-item-action' icon='md-info'/>
-
           </div>
 
         </div>
