d701c5ee746bdc9a44bea26d201866765218a341
Cesar Andreu <cesarandreu@gmail.com>
2015-03-15 17:54:03 -0700

    secret script should let you require it

--- scripts/secret.js
+++ scripts/secret.js
@@ -1,4 +1,14 @@
-require('crypto').randomBytes(64, function (err, buff) {
-  if (err) throw err
-  console.log(buff.toString('hex'))
-})
+// Print to stdout if called directly
+if (require.main === module)
+  generateSecret(function (err, secret) {
+    if (err) throw err
+    console.log(secret)
+  })
+
+module.exports = generateSecret
+
+function generateSecret (cb) {
+  require('crypto').randomBytes(64, function (err, buff) {
+    err ? cb(err) : cb(null, buff.toString('hex'))
+  })
+}
