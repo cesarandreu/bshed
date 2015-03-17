21c7dff5c5fb3cd07b1a57a3b801df4a041ba2b6
Cesar Andreu <cesarandreu@gmail.com>
2015-03-16 21:03:28 -0700

    remove useless quotation marks

--- config/database.js
+++ config/database.js
@@ -1,24 +1,24 @@
 module.exports = {
-  'development': {
-    'username': null,
-    'password': null,
-    'database': 'bshed_development',
-    'host': '127.0.0.1',
-    'dialect': 'postgres'
+  development: {
+    username: null,
+    password: null,
+    database: 'bshed_development',
+    host: '127.0.0.1',
+    dialect: 'postgres'
   },
-  'test': {
-    'username': null,
-    'password': null,
-    'database': 'bshed_test',
-    'host': '127.0.0.1',
-    'dialect': 'postgres',
-    'logging': false
+  test: {
+    username: null,
+    password: null,
+    database: 'bshed_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    logging: false
   },
-  'production': {
-    'username': null,
-    'password': null,
-    'database': 'bshed_production',
-    'host': '127.0.0.1',
-    'dialect': 'postgres'
+  production: {
+    username: null,
+    password: null,
+    database: 'bshed_production',
+    host: '127.0.0.1',
+    dialect: 'postgres'
   }
 }
