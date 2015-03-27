93a203c1bf6f31775eff7ab4cfe84f2a38368602
Cesar Andreu <cesarandreu@gmail.com>
2015-03-27 02:25:24 -0700

    refactor bikeshed builder add action to be cleaner

--- client/actions/BikeshedBuilder.js
+++ client/actions/BikeshedBuilder.js
@@ -1,39 +1,38 @@
-var co = require('co'),
-  _ = require('lodash')
+var co = require('co')
 
 module.exports = {
-  addFiles: co.wrap(function* (context, payload) {
-    var files = Array.from(payload)
-      .filter(file => {
-        var isValid = _.includes(['image/png', 'image/jpeg'], file.type)
-        if (!isValid) // TODO: implement toast handler for invalid types
-          context.dispatch('ADD_TOAST_MESSAGE', `File ${file.name} has invalid type ${file.type}`)
-        return isValid
-      })
-      .map(file => {
-        var url = URL.createObjectURL(file)
-        var name = file.name
-        return {file, url, name}
-      })
+  addFiles: co.wrap(function* addFiles (context, payload) {
+    var fileList = yield Array.from(payload)
+    .filter(file => {
+      var isValid = ['image/png', 'image/jpeg'].indexOf(file.type) > -1
+      if (!isValid)
+        context.dispatch('ADD_TOAST_MESSAGE', `File ${file.name} has invalid type ${file.type}`)
+      return isValid
+    })
+    .map(file => {
+      var name = file.name
+      return imageSize(file).then(size => ({name, file, size}))
+    })
 
-    var sizes = yield files.map(file => imageSize(file.url))
-    files.forEach((file, idx) => file.size = sizes[idx])
-
-    context.dispatch('ADD_BIKES_TO_BIKESHED_BUILDER', files)
+    if (fileList.length)
+      context.dispatch('ADD_BIKES_TO_BIKESHED_BUILDER', fileList)
   }),
 
-  removeBike: function (context, payload) {
+  removeBike: function removeBike (context, payload) {
     context.dispatch('REMOVE_BIKE_FROM_BIKESHED_BUILDER', payload)
   }
 }
 
-function imageSize (url) {
+function imageSize (file) {
   return new Promise((resolve, reject) => {
     var img = document.createElement('img')
-    img.onload = function () {
+    var url = URL.createObjectURL(file)
+    img.onload = function onload () {
+      URL.revokeObjectURL(url)
       resolve({width: img.width, height: img.height})
     }
-    img.onerror = function (err) {
+    img.onerror = function onerror (err) {
+      URL.revokeObjectURL(url)
       reject(err)
     }
     img.src = url
