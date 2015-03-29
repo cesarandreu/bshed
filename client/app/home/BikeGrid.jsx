ebc0ba1283e6600dc7f0b0b143c386fd929a3538
Cesar Andreu <cesarandreu@gmail.com>
2015-03-28 20:29:57 -0700

    readability

--- client/app/home/BikeGrid.jsx
+++ client/app/home/BikeGrid.jsx
@@ -6,17 +6,20 @@ var BikeGrid = React.createClass({
 
   render: function () {
     var {bikes} = this.props
-    var items = bikes.map((bike, key) =>
-      <BikeGridItem key={key} bike={bike} onBikeClick={this.props.onBikeClick}/>
+    var className = classnames(
+      'bike-grid-container grid-container',
+      {'empty': !bikes.length}
     )
 
     return (
-      <div className={classnames('bike-grid-container grid-container', {'empty': !bikes.length})}>
+      <div className={className}>
         <div className='bike-grid-subheader grid-subheader'>
           Bikes
         </div>
         <div className='bike-grid grid'>
-          {items}
+          {bikes.map((bike, key) =>
+            <BikeGridItem key={key} bike={bike} onBikeClick={this.props.onBikeClick}/>
+          )}
         </div>
       </div>
     )
