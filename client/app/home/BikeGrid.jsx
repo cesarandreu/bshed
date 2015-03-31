74e642122dbeee7079fd96764f648dd142791e1c
Cesar Andreu <cesarandreu@gmail.com>
2015-03-30 19:56:39 -0700

    fix bike grid actions and prop types

--- client/app/home/BikeGrid.jsx
+++ client/app/home/BikeGrid.jsx
@@ -3,7 +3,11 @@ var React = require('react'),
   BikeGridItem = require('./BikeGridItem.jsx')
 
 var BikeGrid = React.createClass({
-
+  propTypes: {
+    onBikeClear: React.PropTypes.func.isRequired,
+    onBikeClick: React.PropTypes.func.isRequired,
+    bikes: React.PropTypes.array.isRequired
+  },
   render: function () {
     var {bikes} = this.props
     var className = classnames(
@@ -18,7 +22,11 @@ var BikeGrid = React.createClass({
         </div>
         <div className='bike-grid grid'>
           {bikes.map((bike, key) =>
-            <BikeGridItem key={key} bike={bike} onBikeClick={this.props.onBikeClick}/>
+            <BikeGridItem
+              onBikeClear={this.props.onBikeClear}
+              onBikeClick={this.props.onBikeClick}
+              bike={bike}
+              key={key}/>
           )}
         </div>
       </div>
--- client/app/home/BikeGridItem.jsx
+++ client/app/home/BikeGridItem.jsx
@@ -2,23 +2,30 @@ var React = require('react'),
   IconButton = require('../../components/buttons/IconButton.jsx')
 
 var BikeGridItem = React.createClass({
-
+  propTypes: {
+    onBikeClear: React.PropTypes.func.isRequired,
+    onBikeClick: React.PropTypes.func.isRequired
+  },
   render: function () {
     var {bike} = this.props
     return (
-      <div className='bike-item' onClick={this._onClick}>
+      <div className='bike-item'>
         <div className='bike-item-metadata'>
           <div className='bike-item-name' title={bike.name}>{bike.name}</div>
-          <IconButton className='bike-item-clear' icon='md-clear'/>
+          <IconButton className='bike-item-clear' icon='md-clear' onClick={this._onBikeClear}/>
         </div>
-        <div className='bike-item-image'>
+        <div className='bike-item-image' onClick={this._onBikeClick}>
           <img src={bike.url} alt={bike.file.name}/>
         </div>
       </div>
     )
   },
 
-  _onClick: function () {
+  _onBikeClear: function () {
+    this.props.onBikeClear(this.props.bike.name)
+  },
+
+  _onBikeClick: function () {
     this.props.onBikeClick(this.props.bike.name)
   }
 
