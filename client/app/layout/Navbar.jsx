0c52bfdb9e357e964642188f4d9c811e14d29019
Cesar Andreu <cesarandreu@gmail.com>
2015-04-05 19:23:25 -0700

    Remove NavbarUser

--- client/app/layout/Navbar.jsx
+++ client/app/layout/Navbar.jsx
@@ -2,7 +2,6 @@ var React = require('react'),
   IconButton = require('../../components/buttons/IconButton.jsx'),
   ActionMixin = require('../../utils/mixins/ActionMixin'),
   LayoutAction = require('../../actions/Layout'),
-  NavbarUser = require('./NavbarUser.jsx'),
   {Link} = require('react-router')
 
 var Navbar = React.createClass({
@@ -17,7 +16,6 @@ var Navbar = React.createClass({
         <IconButton className='navbar-menu-button' icon='md-menu' onClick={this.toggleMenu}/>
         <div className='navbar-inner'>
           <Link className='navbar-title' to='home'>Bikeshed it!</Link>
-          <NavbarUser/>
         </div>
       </nav>
     )
