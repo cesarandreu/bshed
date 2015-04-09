42ad74b69c4792b9baa2ca8dee50cc593c37545d
Cesar Andreu <cesarandreu@gmail.com>
2015-04-09 00:37:20 -0700

    Update Dialog and add styles

--- client/components/dialog/Dialog.jsx
+++ client/components/dialog/Dialog.jsx
@@ -1,22 +1,35 @@
-var React = require('react/addons'),
+var cn = require('classnames'),
+  React = require('react/addons'),
+  hotkey = require('react-hotkey'),
   PureRenderMixin = React.addons.PureRenderMixin
 
 var Dialog = React.createClass({
-  mixins: [PureRenderMixin],
+  mixins: [PureRenderMixin, hotkey.Mixin('_handleHotkey')],
 
   render: function () {
+    var {children, className, ...other} = this.props
     return (
-      <div className='dialog-wrapper'>
+      <div className={cn('dialog-wrapper', className)} {...other}>
         <div className='dialog-overlay'></div>
-
-        <div className='dialog-container'>
+        <div className='dialog-container' onClick={this._onClick}>
           <div className='dialog'>
-            {this.props.children}
+            {children}
           </div>
         </div>
       </div>
     )
+  },
+
+  _onClick: function (e) {
+    if (e.target.classList.contains('dialog-wrapper'))
+      this.props.onClose()
+  },
+
+  _handleHotkey: function (e) {
+    if (e.key === 'Escape')
+      this.props.onClose()
   }
+
 })
 
 module.exports = Dialog
