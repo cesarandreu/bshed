702da029e998e30b13c4cb1426046922b1ed6358
Cesar Andreu <cesarandreu@gmail.com>
2015-04-19 20:53:12 -0700

    Update grid to allow subheader

--- client/components/grid/Grid.jsx
+++ client/components/grid/Grid.jsx
@@ -6,14 +6,18 @@ var Grid = React.createClass({
   mixin: [PureRenderMixin],
 
   propTypes: {
-    className: React.PropTypes.string
+    className: React.PropTypes.string,
+    subheader: React.PropTypes.string
   },
 
   render: function () {
-    var {children, className, ...props} = this.props
+    var {children, className, subheader, ...props} = this.props
     return (
-      <div className={cn('grid', className)} {...props}>
-        {children}
+      <div className={cn('grid-container', className)} {...props}>
+        {subheader ? <div className='grid-subheader'>{subheader}</div> : null}
+        <div className='grid'>
+          {children}
+        </div>
       </div>
     )
   }
