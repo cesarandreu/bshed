1d03e4d5b07847fc1b3955edf786ecbc9e6f7e6e
Cesar Andreu <cesarandreu@gmail.com>
2015-04-19 20:55:06 -0700

    Add classess to BikeItem image based on size

--- client/app/home/components/BikeItem.jsx
+++ client/app/home/components/BikeItem.jsx
@@ -1,23 +1,32 @@
-var React = require('react/addons'),
-  PureRenderMixin = React.addons.PureRenderMixin,
-  IconButton = require('../../../components/buttons/IconButton')
+const BIKE_ITEM_SIZE = 160
+var cn = require('classnames')
+var React = require('react/addons')
+var PureRenderMixin = React.addons.PureRenderMixin
+var IconButton = require('../../../components/buttons/IconButton')
 
 var BikeItem = React.createClass({
   mixins: [PureRenderMixin],
 
   propTypes: {
     onBikeClear: React.PropTypes.func.isRequired,
-    // onBikeClick: React.PropTypes.func.isRequired,
     bike: React.PropTypes.object.isRequired
   },
 
   render: function () {
     var {bike} = this.props
 
+    var {width, height} = bike.get('size')
+    var imageClass = cn('bike-item-image', {
+      small: width < BIKE_ITEM_SIZE || height < BIKE_ITEM_SIZE,
+      square: width === height,
+      longer: height > width,
+      wider: width > height
+    })
+
     return (
       <div className='bike-item'>
         <IconButton className='bike-item-clear' icon='md-clear' onClick={this._onBikeClear}/>
-        <div className='bike-item-image'>
+        <div className={cn('bike-item-image', imageClass)}>
           <img src={bike.get('url')} alt={bike.get('file').name}/>
         </div>
       </div>
