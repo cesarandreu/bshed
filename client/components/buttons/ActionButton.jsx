4f28a608bc47af6752ccf849141e3deb14ecc911
Cesar Andreu <cesarandreu@gmail.com>
2015-04-26 14:55:50 -0700

    Spacing and readability of ActionButton

--- client/components/buttons/ActionButton.jsx
+++ client/components/buttons/ActionButton.jsx
@@ -1,19 +1,25 @@
-var React = require('react/addons'),
-  classnames = require('classnames'),
-  IconButton = require('./IconButton'),
-  PureRenderMixin = React.addons.PureRenderMixin
+var cn = require('classnames')
+var React = require('react/addons')
+var IconButton = require('./IconButton')
+var PureRenderMixin = React.addons.PureRenderMixin
 
 var ActionButton = React.createClass({
   mixins: [PureRenderMixin],
+
   propTypes: {
     icon: React.PropTypes.string.isRequired,
     className: React.PropTypes.string,
     mini: React.PropTypes.bool
   },
+
   render: function () {
     var {className, icon, mini, ...props} = this.props
-    className = classnames('action-button', {mini}, className)
-    return <IconButton className={className} icon={icon} {...props}/>
+    return (
+      <IconButton
+        className={cn('action-button', {mini}, className)}
+        icon={icon}
+        {...props}/>
+    )
   }
 })
 
