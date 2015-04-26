eec2c7c7c02ecc35cb33e840703e12172af64460
Cesar Andreu <cesarandreu@gmail.com>
2015-04-26 14:55:18 -0700

    Add secondary and primary colors to CircularPreloader

--- client/components/general/CircularPreloader.jsx
+++ client/components/general/CircularPreloader.jsx
@@ -6,12 +6,16 @@ var CircularPreloader = React.createClass({
   mixins: [PureRenderMixin],
 
   propTypes: {
-    className: React.PropTypes.string
+    className: React.PropTypes.string,
+    secondary: React.PropTypes.bool,
+    primary: React.PropTypes.bool
   },
 
   render: function () {
-    var {className, ...props} = this.props
-    return <div className={cn('circular-preloader', className)} {...props}></div>
+    var {className, primary, secondary, ...props} = this.props
+    return (
+      <div className={cn('circular-preloader', className, {primary, secondary})} {...props}/>
+    )
   }
 })
 
