2774fadbe2bacfdabec416d6ef9a854e20e78de2
Cesar Andreu <cesarandreu@gmail.com>
2015-05-02 15:01:53 -0700

    buildHeaders readability

--- test/buildHeaders.js
+++ test/buildHeaders.js
@@ -1,23 +1,21 @@
-'use strict'
-
 // REFERENCES
 // https://github.com/pillarjs/cookies/blob/master/lib/cookies.js
 // https://github.com/koajs/session/blob/master/index.js
 // https://github.com/koajs/csrf/blob/master/index.js
-var Cookie = require('cookies').Cookie,
-  Keygrip = require('keygrip'),
-  assert = require('assert'),
-  csrf = require('csrf'),
-  _ = require('lodash')
+var Cookie = require('cookies').Cookie
+var Keygrip = require('keygrip')
+var assert = require('assert')
+var csrf = require('csrf')
+var _ = require('lodash')
 
-// secret - array of secrets for Keygrip
 // key - session cookie name
-module.exports = function buildHeadersHelper ({secret, key}={}) {
-  assert(secret && key, 'secret and key are needed')
+// secrets - array of secrets for Keygrip
+module.exports = function buildHeadersHelper ({secrets, key}={}) {
+  assert(secrets && key, 'secrets and key are needed')
 
-  var maxAge = 24 * 60 * 60 * 1000,
-    keys = new Keygrip(secret),
-    tokens = csrf()
+  var maxAge = 24 * 60 * 60 * 1000
+  var keys = new Keygrip(secrets)
+  var tokens = csrf()
 
   return function buildHeaders (session) {
     // _expire and _maxAge are used by koa-session
@@ -25,14 +23,14 @@ module.exports = function buildHeadersHelper ({secret, key}={}) {
       _expire: maxAge + Date.now(),
       _maxAge: maxAge,
       secret: tokens.secretSync()
-    }, session)
+    }, _.cloneDeep(session))
 
-    var value = (new Buffer(JSON.stringify(session))).toString('base64'),
-      cookie = new Cookie(key, value, {maxAge}),
-      headers = {
-        'x-csrf-token': tokens.create(session.secret),
-        'cookie': `${cookie.toHeader()};`
-      }
+    var value = (new Buffer(JSON.stringify(session))).toString('base64')
+    var cookie = new Cookie(key, value, {maxAge})
+    var headers = {
+      'x-csrf-token': tokens.create(session.secret),
+      'cookie': `${cookie.toHeader()};`
+    }
 
     // signed cookie
     cookie.value = keys.sign(cookie.toString())
