a40d4914bdd8bc019fe29aba54e859629a50e58d
Cesar Andreu <cesarandreu@gmail.com>
2015-05-07 22:52:31 -0700

    Readability

--- client/renderer/Html.jsx
+++ client/renderer/Html.jsx
@@ -18,16 +18,22 @@ var Html = React.createClass({
         <head>
           <meta charSet='utf-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge,chrome=1'/>
-          <meta name='viewport'
-            content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' />
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'
+          />
           <meta name='description' content='An app for bikeshedding'/>
           <title>{title}</title>
-          {styles.map((href, key) => <link href={href} key={key} rel='stylesheet'></link>)}
+          {styles.map((href, key) =>
+            <link href={href} key={key} rel='stylesheet'></link>
+          )}
         </head>
         <body>
           <div id='bshed' dangerouslySetInnerHTML={{__html: this.props.markup}}></div>
           <script dangerouslySetInnerHTML={{__html: this.props.BSHED}}></script>
-          {scripts.map((src, key) => <script src={src} key={key}></script>)}
+          {scripts.map((src, key) =>
+            <script src={src} key={key}></script>
+          )}
         </body>
       </html>
     )
