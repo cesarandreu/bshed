3c81d0197f6fad325e92c1976a2a13b8c2261d27
Cesar Andreu <cesarandreu@gmail.com>
2015-05-13 16:13:20 +0900

    Add Rating model

---
+++ models/rating.js
@@ -0,0 +1,39 @@
+module.exports = function (sequelize, DataTypes) {
+  var Rating = sequelize.define('Rating', {
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    value: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 0
+      }
+    },
+    BikeId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      validate: {
+        isUUID: true
+      }
+    },
+    VoteId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      validate: {
+        isUUID: true
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function associate (models) {
+        models.Rating.belongsTo(models.Bike)
+        models.Rating.belongsTo(models.Vote)
+      }
+    }
+  })
+
+  return Rating
+}
