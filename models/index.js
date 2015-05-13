5778c71d1e29f12f1264aae0164efc836c9394a0
Cesar Andreu <cesarandreu@gmail.com>
2015-05-13 16:12:53 +0900

    Update model loader

--- models/index.js
+++ models/index.js
@@ -1,32 +1,38 @@
-var Sequelize = require('sequelize'),
-  path = require('path'),
-  fs = require('fs')
-
-var assert = require('assert'),
-  debug = require('debug')('bshed:models')
+var debug = require('debug')('bshed:models')
+var Sequelize = require('sequelize')
+var assert = require('assert')
+var path = require('path')
+var fs = require('fs')
 
 /**
  * Model loader
- *
- * requires opts.config
- * returns models object
+ * @param {Object} opts
+ * @param {Object} opts.config
+ * @returns {Object} Models
  */
 module.exports = function modelsLoader ({config}={}) {
-  assert(config)
+  assert(config, 'model loader requires config')
   debug('loader:start')
 
-  var models = {},
-    sequelize = new Sequelize(config.database, config.username, config.password, config)
+  var models = {}
+  var sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  )
 
   // Load models
   fs.readdirSync(__dirname)
-  .filter(file => file.indexOf('.') !== 0 && file !== 'index.js' && file !== 'test')
-  .forEach(file => {
-    var modelPath = path.join(__dirname, file)
-    var model = sequelize.import(modelPath)
-    models[model.name] = model
-    debug(`${model.name} loaded from file ${modelPath}`)
-  })
+    .filter(file =>
+      file.indexOf('.') !== 0 && file !== 'index.js' && file !== 'test'
+    )
+    .forEach(file => {
+      var modelPath = path.join(__dirname, file)
+      var model = sequelize.import(modelPath)
+      models[model.name] = model
+      debug(`${model.name} loaded from file ${modelPath}`)
+    })
 
   // Associate and initialize models
   Object.keys(models).forEach(name => {
