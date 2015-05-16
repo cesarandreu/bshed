ad7ba425599db211e251a75ee0bea7875361cb84
Cesar Andreu <cesarandreu@gmail.com>
2015-05-16 09:27:02 +0900

    Cleaning up server

--- server.js
+++ server.js
@@ -1,75 +1,76 @@
-require('babel/register')({
-  ignore: /node_modules|public/
-})
-
 // modules
 var koa = require('koa')
 var qs = require('koa-qs')
 var csrf = require('koa-csrf')
-var mount = require('koa-mount')
-var serve = require('koa-static')
 var session = require('koa-session')
 var compress = require('koa-compress')
 var debug = require('debug')('bshed:server')
+var middleware = require('./utils/middleware')
 var responseTime = require('koa-response-time')
 
-// application modules
-debug('loading modules')
-var config = require('./config')
-var models = require('./models')
-var s3 = require('./lib/s3')
-var api = require('./api')
-var client = require('./client/renderer/middleware')
+// libs and modules
+debug('loading libs and modules')
+var modelLoader = require('./models')
+var s3Loader = require('./lib/s3')
+var apiLoader = require('./api')
+
+// configuration
+debug('loading and generating config')
+var configGenerator = require('./config')
+var config = configGenerator({})
 
 // initialization
-debug('initializing modules')
-s3 = s3(config.aws)
-models = models({
-  config: config.database
-})
-api = api({
-  config: config.api,
-  models: models,
-  s3: s3
-})
-client = client({
-  assetPath: config.server.assets
-})
+debug('initializing libs and modules')
+var s3 = s3Loader(config.aws)
+var models = modelLoader(config)
+var api = apiLoader({config, models, s3})
 
 /**
  * SERVER
+ * Exported so it can be started externally
  */
-var server = qs(koa())
-server.name = config.server.name
-server.env = config.server.env
-server.keys = config.secret
-server.config = config
-server.models = models
-server.api = api
+var server = module.exports = qs(koa())
+Object.assign(server, {
+  name: config.name,
+  keys: config.keys,
+  env: config.env,
+  config,
+  models,
+  api,
+  s3
+})
 
 /**
  * MIDDLEWARE
  */
-if (server.env === 'development')
-  server.use(require('koa-logger')()) // request logging
-server.use(responseTime()) // x-response-time
-server.use(compress()) // compression
-server.use(serve(config.server.assets, config.server.middleware.serve)) // assets
-server.use(session(config.server.middleware.session, server)) // sessions
-server.use(csrf()) // csrf
 
-// set csrf cookie token
-server.use(function* setCsrfCookie (next) {
-  this.cookies.set('XSRF-TOKEN', this.csrf)
-  yield next
-})
+// request logging
+if (server.env === 'development')
+  server.use(require('koa-logger')())
+
+// x-response-time
+server.use(responseTime())
+
+// compression
+server.use(compress())
+
+// cookie sessions
+server.use(session(config.middleware.session, server))
+
+// csrf token
+server.use(csrf())
+
+// XSRF-TOKEN
+server.use(middleware.setCsrfToken())
+
+// logged_in cookie
+server.use(middleware.setLoggedInCookie())
 
 /**
- * APPLICATIONS
+ * APPLICATION
  */
 debug('mounting modules')
-server.use(mount(api))
-server.use(mount(client))
+server.use(api)
 
 /**
  * SERVER INITIALIZER
@@ -77,14 +78,11 @@ server.use(mount(client))
  * Listen for connections
  * returns server instance
  */
-server.init = function init (port=config.server.port) {
-  debug('initializing server using port %d', port)
-  server.server = server.listen(port, () => debug('listening on port %d', port))
+server.init = function init (port=config.port) {
+  debug(`initializing server using port ${port}`)
+  server.server = server.listen(port, () => debug(`listening on port ${port}`))
   return server.server
 }
-
-// export the server so it can be started externally
-module.exports = server
 
 // initialize server if called directly
 if (require.main === module)
