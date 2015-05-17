87c85792f3713273d148db50c2c39fe41e8cd42f
Cesar Andreu <cesarandreu@gmail.com>
2015-05-17 17:54:20 +0900

    Simplify and document middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,11 +1,9 @@
 var assert = require('assert')
-var _ = require('lodash')
 
 /**
- * addToContext
  * Adds all enumerable keys in objects to context
  * @param {Object} [objects={}]
- * @returns {Function} addToContextMiddleware
+ * @returns {Middleware} addToContextMiddleware
  */
 exports.addToContext = function addToContext (objects={}) {
   return function* addToContextMiddleware (next) {
@@ -15,26 +13,25 @@ exports.addToContext = function addToContext (objects={}) {
 }
 
 /**
- * auth
- *
  * Loads user to ctx.state.user using session user.id
- * Throws 401 if session is not set or user isn't found
+ * Throw 401 if session isn't set or user isn't found
+ * @returns {Middleware} authenticateMiddleware
  */
-exports.authenticate = function authenticate ({skippable=false}={}) {
+exports.authenticate = function authenticate () {
   return function* authenticateMiddleware (next) {
     try {
       this.state.user = yield this.models.User.find(this.session.user.id)
-      if (!this.state.user) throw new Error('user not found')
-    } catch (err) {
-      if (!skippable) this.throw(401)
+    } finally {
+      if (!this.state.user)
+        this.throw(401)
     }
     yield next
   }
 }
 
 /**
- * errorHandler
  * Handle Joi and Boom errors
+ * @returns {Middleware} errorHandlerMiddleware
  */
 exports.errorHandler = function errorHandler () {
   return function* errorHandlerMiddleware (next) {
@@ -59,65 +56,54 @@ exports.errorHandler = function errorHandler () {
 }
 
 /**
- * load
- * Searches for :name param using resource model and sets value to ctx.state[name]
- * Throws 404 if search fails
- *
- * param resource - model name
- * param opts.name - param value to use. default: resource.toLowerCase()
- * param opts.key - searching value to use. default: id
- * param opts.parent - include parent in where, like resource
- * param opts.parent.resource - model name
- * param opts.parent.name - param value to use. default: parent.resource.toLowerCase()
- * param opts.parent.through - association key to use. default: ${parent.resource}Id
- * param opts.parent.key - parent.use key to check. default: id
- *
- * example load('Bikeshed') - uses Bikeshed model, :bikeshed param, and sets ctx.state.bikeshed
+ * Searches for name param using resource model
+ * Sets result to ctx.state[name]
+ * Throws 404 on failure
+ * @example
+ * // Uses Bikeshed model, bikeshed param, sets ctx.state.bikeshed
+ * load('Bikeshed')
+ * @param {String} resource Model name
+ * @param {Object} [opts={}] Options
+ * @param {String} [opts.name=resource.toLowerCase()] Param value to check
+ * @param {String} [opts.key='id'] Searching value to use
+ * @returns {Middleware} Model loading middleware
  */
-exports.load = function load (resource, {parent, key='id', name=resource.toLowerCase()}={}) {
+exports.load = function load (resource, {key='id', name=resource.toLowerCase()}={}) {
   assert(resource, 'load middleware requires a resource')
-  if (parent) {
-    if (_.isString(parent)) parent = {resource: parent}
-    parent = _.assign({
-      name: parent.resource.toLowerCase(),
-      through: `${parent.resource}Id`,
-      key: 'id'
-    }, parent)
-  }
-
   return function* loadMiddleware (next) {
-    // inside of try-catch in case any key is incorrect
-    try {
-      var params = {
-        where: {[key]: this.params[name]}
+    this.state[name] = yield this.models[resource].find({
+      where: {
+        [key]: this.params[name]
       }
-      if (parent) params.where[parent.through] = this.state[parent.name][parent.key]
-      this.state[name] = yield this.models[resource].find(params)
-      if (!this.state[name]) throw new Error(`${resource} not found`)
-    } catch (err) {
+    })
+
+    if (!this.state[name])
       this.throw(404, `${resource} not found`)
-    }
+
     yield next
   }
 }
 
 /**
- * setCsrfToken
  * Middleware to set XSRF-TOKEN cookie on every response
- * Session check is needed in case it's null
+ * @returns {Middleware}
  */
 exports.setCsrfToken = function setCsrfToken () {
   return function* setCsrfCookieMidleware (next) {
     yield next
+
+    // Session check needed in case it's null
     if (this.session)
-      this.cookies.set('XSRF-TOKEN', this.csrf, {httpOnly: false})
+      this.cookies.set('XSRF-TOKEN', this.csrf, {
+        httpOnly: false
+      })
   }
 }
 
 /**
- * setLoggedInCookie
  * Middleware to set logged_in cookie on every response
  * Used by client to try to guess allowed states
+ * @returns {Middleware}
  */
 exports.setLoggedInCookie = function setLoggedInCookie () {
   return function* setLoggedInCookieMiddleware (next) {
