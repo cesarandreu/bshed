541bc9456d8bb0a8355268021134b5454e25f280
Cesar Andreu <cesarandreu@gmail.com>
2015-05-18 11:30:26 +0900

    Update AWS api version

--- config/aws.js
+++ config/aws.js
@@ -3,21 +3,21 @@ module.exports = {
   development: {
     accessKeyId: 'ACCESS_KEY_ID',
     secretAccessKey: 'SECRET_ACCESS_KEY',
-    apiVersion: '2014-11-23',
+    apiVersion: '2015-05-18',
     s3ForcePathStyle: true,
     endpoint: new AWS.Endpoint('http://localhost:10001')
   },
   test: {
     accessKeyId: 'ACCESS_KEY_ID',
     secretAccessKey: 'SECRET_ACCESS_KEY',
-    apiVersion: '2014-11-23',
+    apiVersion: '2015-05-18',
     s3ForcePathStyle: true,
     endpoint: new AWS.Endpoint('http://localhost:10001')
   },
   production: {
     accessKeyId: '',
     secretAccessKey: '',
-    apiVersion: '2014-11-23',
+    apiVersion: '2015-05-18',
     s3ForcePathStyle: true,
     endpoint: new AWS.Endpoint('http://localhost:10001')
   }
