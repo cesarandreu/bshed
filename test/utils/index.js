e778b0827d47fd7f3adae062ee932036b1203239
Cesar Andreu <cesarandreu@gmail.com>
2015-05-25 01:10:16 -0700

    Add test utils

---
+++ test/utils/index.js
@@ -0,0 +1,18 @@
+const buildHeaders = require('./build-headers')
+
+module.exports = {
+  buildUserHeaders,
+  buildHeaders
+}
+
+function buildUserHeaders (opts) {
+  const _buildHeaders = buildHeaders(opts)
+  return function (user) {
+    return _buildHeaders({
+      user: {
+        id: user.id,
+        name: user.name
+      }
+    })
+  }
+}
