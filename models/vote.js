e8eaa1149d3b4d98e61a454d1cd9e06f2a458f5c
Cesar Andreu <cesarandreu@gmail.com>
2015-05-25 01:10:02 -0700

    Add before and after create hooks to Vote

--- models/vote.js
+++ models/vote.js
@@ -1,3 +1,5 @@
+const createError = require('http-errors')
+
 module.exports = function (sequelize, DataTypes) {
   const Vote = sequelize.define('Vote', {
     id: {
@@ -14,6 +16,17 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false
     }
   }, {
+    hooks: {
+      async beforeCreate (vote, opts) {
+        // Confirm vote count is zero
+        await confirmVoteCount(vote, opts, 0)
+      },
+
+      async afterCreate (vote, opts) {
+        // Confirm vote count is one
+        await confirmVoteCount(vote, opts, 1)
+      }
+    },
     classMethods: {
       associate (models) {
         Vote.belongsTo(models.Bikeshed)
@@ -24,4 +37,23 @@ module.exports = function (sequelize, DataTypes) {
   })
 
   return Vote
+
+  /**
+   * Confirm Vote.count matches expected value
+   * @param {Instance} vote Model instance
+   * @param {Object} opts Model options
+   * @param {Number} value Expected count
+   * @returns {Promise}
+   */
+  async function confirmVoteCount (vote, opts, value) {
+    const voteCount = await Vote.count({
+      transaction: opts.transaction,
+      where: {
+        BikeshedId: vote.BikeshedId,
+        UserId: vote.UserId
+      }
+    })
+    if (voteCount !== value)
+      throw createError(409, 'Can only vote once per Bikeshed', {expose: true})
+  }
 }
