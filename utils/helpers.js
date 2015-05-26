824d63ca09001e244b8756bc18c54f10d591ce0f
Cesar Andreu <cesarandreu@gmail.com>
2015-05-26 00:23:25 -0700

    Update helpers docs

--- utils/helpers.js
+++ utils/helpers.js
@@ -8,6 +8,11 @@ const createError = require('http-errors')
  * retry
  * ripped off from co-retry
  * Execute fn and retry on failure
+ * @param {Function} fn Function to retry
+ * @param {Object} [opts={}] Options
+ * @param {number} [attempts=3] Number of times to try
+ * @param {number} [interval=300] Time interval to wait
+ * @param {number} [delta=150] Range in which time interval can vary
  */
 exports.retry = function* retry (fn, {attempts=3, interval=300, delta=150}={}) {
   while (true)
@@ -23,7 +28,7 @@ exports.retry = function* retry (fn, {attempts=3, interval=300, delta=150}={}) {
  * Check schema for object
  * @param {Object} object Object to validate
  * @param {Object} schema Schema to use in validation
- * @returns {Proimise} Schema validation promise
+ * @returns {Promise<Object>} Schema validation promise
  */
 exports.checkSchema = function checkSchema (object, schema) {
   return new Promise((resolve, reject) => {
