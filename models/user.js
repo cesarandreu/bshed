c2df6c0aa11eb47e349232a962c39daf261ff623
Cesar Andreu <cesarandreu@gmail.com>
2015-05-26 01:48:19 -0700

    Add checkPassword to user model instance

--- models/user.js
+++ models/user.js
@@ -44,12 +44,27 @@ module.exports = function (sequelize, DataTypes) {
       roles: false
     }
   }, {
+    instanceMethods: {
+      /**
+       * Check if password is valid or not
+       * @param {string} password Password to check
+       * @returns {Promise<boolean>} Promise of password validity
+       */
+      checkPassword (password) {
+        return User.comparePassword(password, this.getDataValue('hashedPassword'))
+      }
+    },
     classMethods: {
       associate (models) {
         User.hasMany(models.Bikeshed)
         User.hasMany(models.Vote)
       },
 
+      /**
+       * Get hashed password
+       * @param {string} password Password to hash
+       * @returns {Promise<string>} Promise of hashed password
+       */
       hashPassword (password) {
         return new Promise((resolve, reject) => {
           bcrypt.hash(password, 8, (err, hash) => {
@@ -58,6 +73,12 @@ module.exports = function (sequelize, DataTypes) {
         })
       },
 
+      /**
+       * Compare a password and hash
+       * @param {string} password Password to check
+       * @param {string} hash Hash to compare with
+       * @param {Promise<boolean>} Promise of password validity
+       */
       comparePassword (password, hash) {
         return new Promise((resolve, reject) => {
           bcrypt.compare(password, hash, (err, res) => {
