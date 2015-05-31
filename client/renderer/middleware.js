145269d0f90d3961cf51a088e8827db4ebe3746f
Cesar Andreu <cesarandreu@gmail.com>
2015-05-31 12:02:00 -0700

    Fix renderer middleware

--- client/renderer/middleware.js
+++ client/renderer/middleware.js
@@ -1,52 +1,22 @@
-var path = require('path'),
-  renderer = require('./renderer'),
-  log = require('debug')('bshed:client:middleware')
+const renderer = require('./renderer')
+const log = require('debug')('bshed:client:middleware')
 
-module.exports = function ({assetPath}={}) {
-  var assets = assetList(assetPath)
-
-  return function* client () {
+module.exports = function clientRenderer ({assets}={}) {
+  return function* clientRendererMiddleware () {
     try {
-      var {body, type, status} = yield renderer({
-        assets: assets, url: this.url, host: this.host, protocol: this.protocol,
-        csrf: this.csrf, cookie: this.get('cookie')
+      log('starting renderer')
+      const result = yield renderer({
+        url: this.url,
+        assets: assets,
+        cookie: this.get('cookie'),
+        rootUrl: `${this.protocol}://${this.host}`
       })
-      Object.assign(this, {status, body, type})
+
+      log('assigning renderer result')
+      Object.assign(this, result)
     } catch (err) {
       console.error(err)
       this.throw(500)
     }
   }
-}
-
-// asset link loader
-function assetList (assetPath) {
-  var assets = {scripts: [], styles: []}, stats
-
-  try {
-    log(`Getting asset list from ${assetPath}`)
-    stats = require(path.join(assetPath, 'assets/stats.json'))
-  } catch (err) {
-    console.warn(`UNABLE TO LOAD ${path.join(assetPath, 'assets/stats.json')}`)
-    stats = {assetsByChunkName: {bshed: ''}, publicPath: ''}
-  }
-
-  if (typeof stats.assetsByChunkName.bshed === 'string')
-    stats.assetsByChunkName.bshed = [stats.assetsByChunkName.bshed]
-
-  stats.assetsByChunkName.bshed
-  .filter(asset => !asset.indexOf('bshed'))
-  .forEach(asset => {
-    switch (path.extname(asset)) {
-      case '.js':
-        assets.scripts.push(`${stats.publicPath}${asset}`)
-        break
-      case '.css':
-        assets.styles.push(`${stats.publicPath}${asset}`)
-        break
-    }
-  })
-
-  log(`Asset list: ${JSON.stringify(assets)}`)
-  return assets
 }
