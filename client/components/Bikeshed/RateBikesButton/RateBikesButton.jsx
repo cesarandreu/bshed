1fbd9d573ef8f2e4dde463350e078a0b54aeddf8
Cesar Andreu <cesarandreu@gmail.com>
2015-06-02 20:39:05 -0700

    Add rate bikes button

---
+++ client/components/Bikeshed/RateBikesButton/RateBikesButton.jsx
@@ -0,0 +1,31 @@
+require('./rate-bikes-button.less')
+
+const React = require('react/addons')
+const PureRenderMixin = React.addons.PureRenderMixin
+const ActionMixin = require('../../../lib/ActionMixin')
+const BikeshedActions = require('../../../actions/BikeshedActions')
+const LabeledActionButton = require('../../general/Buttons/LabeledActionButton')
+
+const RateBikesButton = React.createClass({
+  mixins: [
+    ActionMixin,
+    PureRenderMixin
+  ],
+
+  render () {
+    return (
+      <LabeledActionButton
+        className='rate-bikes-button'
+        label='Rate bikes'
+        icon='md-directions-bike'
+        onClick={this._onClick}
+      />
+    )
+  },
+
+  _onClick () {
+    // this.executeAction(BikeshedActions)
+  }
+})
+
+module.exports = RateBikesButton
