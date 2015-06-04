45c059885364499f463d3d3176d6776fbb8ca7f8
Cesar Andreu <cesarandreu@gmail.com>
2015-06-03 19:27:24 -0700

    Simplify test helper

--- client/lib/test-helper.js
+++ client/lib/test-helper.js
@@ -1,18 +1,13 @@
-module.exports = {
-  testdom,
-  cleanup
-}
-
-function testdom (markup='') {
+exports.testdom = function testdom (markup='') {
   if (typeof document === 'undefined') {
-    const jsdom = require('jsdom').jsdom
+    const {jsdom} = require('jsdom')
     global.document = jsdom(markup)
     global.window = document.defaultView
     global.navigator = window.navigator
   }
 }
 
-function cleanup () {
+exports.cleanup = function cleanup () {
   const React = require('react')
   React.unmountComponentAtNode(document.body)
   document.body.innerHTML = ''
