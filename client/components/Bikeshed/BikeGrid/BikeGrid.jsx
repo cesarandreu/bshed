6f2b6c83347c49334ad923874395d84f68a1f2c0
Cesar Andreu <cesarandreu@gmail.com>
2015-06-03 19:27:01 -0700

    Add BikeGrid WIP

--- client/components/Bikeshed/BikeGrid/BikeGrid.jsx
+++ client/components/Bikeshed/BikeGrid/BikeGrid.jsx
@@ -1,7 +1,10 @@
 require('./bike-grid.less')
 
+const BIKE_ITEM_SIZE = 160
+const cn = require('classnames')
 const React = require('react/addons')
 const Immutable = require('immutable')
+const Grid = require('../../general/Grid')
 const PureRenderMixin = React.addons.PureRenderMixin
 
 const BikeGrid = React.createClass({
@@ -15,9 +18,37 @@ const BikeGrid = React.createClass({
   },
 
   render () {
+    const {bikeshed, bikes} = this.props
+
     return (
-      <div className='bike-grid'>
-      </div>
+      <Grid className='bike-grid'>
+        {bikeshed.get('Bikes').map(id => {
+          const bike = bikes.get(id)
+          const width = bike.get('width')
+          const height = bike.get('height')
+
+          const imageContainerClassName = cn('bike-grid-item-image-container', {
+            small: width < BIKE_ITEM_SIZE || height < BIKE_ITEM_SIZE,
+            square: width === height,
+            longer: height > width,
+            wider: width > height
+          })
+
+          return (
+            <div className='bike-grid-item' key={id}>
+              <div className={imageContainerClassName}>
+                <img
+                  className='bike-grid-item-image'
+                  alt={bike.get('name')}
+                  src={`http://localhost:10001/bshed/${bikeshed.get('id')}/${id}`}
+                />
+              </div>
+
+            </div>
+          )
+
+        })}
+      </Grid>
     )
   }
 })
