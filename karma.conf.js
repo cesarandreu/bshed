3babf3c02f5140e842640d87d44d6c37992e5d5c
Cesar Andreu <cesarandreu@gmail.com>
2015-06-04 21:13:23 -0700

    Add karma config

---
+++ karma.conf.js
@@ -0,0 +1,32 @@
+var webpackConfig = require('./webpack.karma')
+
+module.exports = function karmaConfig (config) {
+  config.set({
+    frameworks: ['mocha'],
+
+    reporters: ['spec'],
+
+    files: [
+      'node_modules/phantomjs-polyfill/bind-polyfill.js',
+      'client/**/*.test.*'
+    ],
+
+    preprocessors: {
+      'client/**/*.test.*': ['webpack', 'sourcemap']
+    },
+
+    browsers: ['PhantomJS'],
+
+    singleRun: true,
+
+    plugins: [
+      require('karma-mocha'),
+      require('karma-webpack'),
+      require('karma-spec-reporter'),
+      require('karma-sourcemap-loader'),
+      require('karma-phantomjs-launcher')
+    ],
+
+    webpack: webpackConfig
+  })
+}
