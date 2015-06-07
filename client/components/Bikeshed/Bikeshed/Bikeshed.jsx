ca61a4445f9663b165e9a21e2311bcc93f0da4e9
Cesar Andreu <cesarandreu@gmail.com>
2015-06-07 09:55:11 -0700

    Move Bikeshed component

--- client/components/Bikeshed/Bikeshed/Bikeshed.jsx
+++ client/components/Bikeshed/Bikeshed/Bikeshed.jsx
@@ -1,18 +1,17 @@
-require('./Bikeshed.less')
-
 const React = require('react/addons')
 const Immutable = require('immutable')
-const BikeGrid = require('./BikeGrid')
-const BikeshedInfo = require('./BikeshedInfo')
+const BikeGrid = require('../BikeGrid')
+const BikeshedInfo = require('../BikeshedInfo')
 const PureRenderMixin = React.addons.PureRenderMixin
-const BikePreview = require('../shared/BikePreview')
-const ActionMixin = require('../../lib/ActionMixin')
 const {connectToStores} = require('fluxible/addons')
-const RateBikesButton = require('./RateBikesButton')
-const BikeshedStore = require('../../stores/BikeshedStore')
-const BikeshedActions = require('../../actions/BikeshedActions')
+const RateBikesButton = require('../RateBikesButton')
+const BikePreview = require('../../shared/BikePreview')
+const ActionMixin = require('../../../lib/ActionMixin')
+const RegularPage = require('../../general/RegularPage')
+const BikeshedStore = require('../../../stores/BikeshedStore')
+const BikeshedActions = require('../../../actions/BikeshedActions')
 
-const Bikeshed = React.createClass({
+var Bikeshed = React.createClass({
   mixins: [
     ActionMixin,
     PureRenderMixin
@@ -23,20 +22,21 @@ const Bikeshed = React.createClass({
   },
 
   propTypes: {
+    bikes: React.PropTypes.instanceOf(Immutable.OrderedMap).isRequired,
     bikeshed: React.PropTypes.instanceOf(Immutable.Map).isRequired,
-    bikes: React.PropTypes.instanceOf(Immutable.Map).isRequired,
     user: React.PropTypes.instanceOf(Immutable.Map).isRequired,
-    preview: React.PropTypes.instanceOf(Immutable.Map)
+    preview: React.PropTypes.string.isRequired
   },
 
   render () {
     const {bikeshed, preview, bikes, user} = this.props
     return (
-      <div className='bikeshed'>
+      <RegularPage>
         <BikeshedInfo
           bikeshed={bikeshed}
           user={user}
         />
+
         <BikeGrid
           bikeshed={bikeshed}
           bikes={bikes}
@@ -45,10 +45,11 @@ const Bikeshed = React.createClass({
         <RateBikesButton/>
 
         <BikePreview
+          name={preview}
+          url={bikes.getIn([preview, 'url'], '')}
           onClose={this._closePreview}
-          preview={preview}
         />
-      </div>
+      </RegularPage>
     )
   },
 
@@ -57,10 +58,9 @@ const Bikeshed = React.createClass({
   }
 })
 
-module.exports = connectToStores(Bikeshed, [BikeshedStore], stores => {
+Bikeshed = connectToStores(Bikeshed, [BikeshedStore], stores => {
   const {bikeshed, user, bikes} = stores.BikeshedStore.getCurrent()
   const preview = stores.BikeshedStore.getPreview()
-
   return {
     bikeshed,
     preview,
@@ -68,3 +68,5 @@ module.exports = connectToStores(Bikeshed, [BikeshedStore], stores => {
     user
   }
 })
+
+module.exports = Bikeshed
