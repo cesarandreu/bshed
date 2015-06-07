6b045d691f2552153fbee564312e050d82b841b0
Cesar Andreu <cesarandreu@gmail.com>
2015-06-07 09:46:22 -0700

    Clean up Grid

--- client/components/general/Grid/Grid.jsx
+++ client/components/general/Grid/Grid.jsx
@@ -5,17 +5,21 @@ const React = require('react/addons')
 const PureRenderMixin = React.addons.PureRenderMixin
 
 const Grid = React.createClass({
-  mixin: [PureRenderMixin],
+  mixin: [
+    PureRenderMixin
+  ],
 
   propTypes: {
-    className: React.PropTypes.string,
     subheader: React.PropTypes.string
   },
 
   render () {
     const {children, className, subheader, ...props} = this.props
     return (
-      <div className={cn('grid-container', className)} {...props}>
+      <div
+        className={cn('grid-container', className)}
+        {...props}
+      >
         {subheader ? <div className='grid-subheader'>{subheader}</div> : null}
         <div className='grid'>
           {children}
