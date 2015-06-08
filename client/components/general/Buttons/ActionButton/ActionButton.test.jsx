683d58358bc10ed6fd5c2359dc41c024d1aab527
Cesar Andreu <cesarandreu@gmail.com>
2015-06-07 19:25:56 -0700

    Fix and move ActionButton test

---
+++ client/components/general/Buttons/ActionButton/ActionButton.test.jsx
@@ -0,0 +1,18 @@
+const {testdom, cleanup} = require('../../../../lib/test-helper')
+const ActionButton = require('../ActionButton')
+const React = require('react/addons')
+const expect = require('expect.js')
+const {TestUtils} = React.addons
+
+testdom('<html><body></body></html>')
+describe('Component:ActionButton', function () {
+  afterEach(cleanup)
+
+  it('sets correct classNames', function () {
+    const button = TestUtils.renderIntoDocument(<ActionButton icon='foo'/>)
+    expect(React.findDOMNode(button).className).to.contain('action-button')
+
+    const icon = TestUtils.findRenderedDOMComponentWithClass(button, 'icon')
+    expect(React.findDOMNode(icon).className).to.contain('foo')
+  })
+})
