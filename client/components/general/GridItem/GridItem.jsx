3945a68e605b9ebbbd9070081a0b933e6ad47419
Cesar Andreu <cesarandreu@gmail.com>
2015-06-07 19:11:24 -0700

    Add height and width props to GridItem

--- client/components/general/GridItem/GridItem.jsx
+++ client/components/general/GridItem/GridItem.jsx
@@ -9,10 +9,23 @@ const GridItem = React.createClass({
     PureRenderMixin
   ],
 
+  propTypes: {
+    height: React.PropTypes.number,
+    width: React.PropTypes.number
+  },
+
+  getDefaultProps () {
+    return {
+      height: 212,
+      width: 212
+    }
+  },
+
   render () {
-    const {children, className, ...props} = this.props
+    const {children, className, height, width, ...props} = this.props
     return (
       <div
+        style={{height, width}}
         className={cn('grid-item', className)}
         {...props}
       >
