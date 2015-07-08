1b794faa9b4eacf358519f4561d92439a8a8af18
Cesar Andreu <cesarandreu@gmail.com>
2015-07-07 21:14:53 -0700

    Add bikeshed builder route

--- client/components/routes.jsx
+++ client/components/routes.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Route } from 'react-router'
 
+import BikeshedBuilder from './BikeshedBuilder'
 import Layout from './Layout'
 import About from './About'
 import Home from './Home'
 
 const routes = (
-  <Route path= '/' component={Layout}>
-    <Route path='about' component={About}/>
-    <Route path='home' component={Home}/>
+  <Route component={Layout}>
+    <Route path= '/' component={BikeshedBuilder}/>
+    <Route path='/about' component={About}/>
+    <Route path='/home' component={Home}/>
   </Route>
 )
 
