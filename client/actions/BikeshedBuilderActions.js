472f7769c185459ea53c4f997b8d735557dbb418
Cesar Andreu <cesarandreu@gmail.com>
2015-07-08 02:41:32 -0700

    Add additional bikeshed builder actions

--- client/actions/BikeshedBuilderActions.js
+++ client/actions/BikeshedBuilderActions.js
@@ -1,4 +1,5 @@
 import BikeshedBuilderConstants from '../constants/BikeshedBuilderConstants'
+import browserImageSize from 'browser-image-size'
 
 export function navigateAction (context) {
   context.dispatch(BikeshedBuilderConstants.RESET)
@@ -6,4 +7,32 @@ export function navigateAction (context) {
 
 export function inputChange (context, payload) {
   context.dispatch(BikeshedBuilderConstants.INPUT_CHANGE, payload)
+}
+
+export function submit () {
+  console.warn('@TODO submit')
+}
+
+const { FileList, File } = global
+export async function addImages (context, images: FileList) {
+  images = await Promise.all(
+    Array.from(images).map(getImageSize)
+  )
+
+  context.dispatch(BikeshedBuilderConstants.ADD_IMAGES, images)
+}
+
+function getImageSize (file: File) {
+  return browserImageSize(file)
+    .then(({ width, height }) => {
+      return { file, width, height }
+    })
+}
+
+export function removeImage (context, image: string) {
+  console.warn('@TODO addImages')
+}
+
+export function preview (context, image: string) {
+  console.warn('@TODO addImages')
 }
