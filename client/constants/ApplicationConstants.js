3d60c4e4dd0b00ee9ad66504fcfd956cc59e5711
Cesar Andreu <cesarandreu@gmail.com>
2015-07-07 21:07:12 -0700

    Update app constants

--- client/constants/ApplicationConstants.js
+++ client/constants/ApplicationConstants.js
@@ -1,6 +1,6 @@
-import constantCreator from '../lib/constantCreator'
-
-const ApplicationConstants = constantCreator('APPLICATION', {
-})
+const ApplicationConstants = {
+  MAXIMUM_IMAGE_COUNT: 5,
+  MINIMUM_IMAGE_COUNT: 2
+}
 
 export default ApplicationConstants
