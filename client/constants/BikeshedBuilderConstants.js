82cd6ce92f1e2496b715e3e80ab19cfd2294d47a
Cesar Andreu <cesarandreu@gmail.com>
2015-07-07 21:12:00 -0700

    Add bikeshed builder constants

--- client/constants/BikeshedBuilderConstants.js
+++ client/constants/BikeshedBuilderConstants.js
@@ -1,6 +1,13 @@
 import constantCreator from '../lib/constantCreator'
 
 const BikeshedBuilderConstants = constantCreator('BIKESHED_BUILDER', {
+  RESET: null,
+  ERROR: null,
+  SUBMIT: null,
+  PREVIEW: null,
+  ADD_IMAGES: null,
+  REMOVE_IMAGE: null,
+  INPUT_CHANGE: null
 })
 
 export default BikeshedBuilderConstants
