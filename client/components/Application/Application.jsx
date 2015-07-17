be8ffd8a1cd75b9e4aef632d2586dd9d69df2a33
Cesar Andreu <cesarandreu@gmail.com>
2015-07-16 19:05:59 -0700

    Experimenting with react-router

--- client/components/Application/Application.jsx
+++ client/components/Application/Application.jsx
@@ -1,26 +1,32 @@
 // import './Application.less'
 
+import navigateAction from '../../actions/navigateAction'
 import { provideContext } from 'fluxible-addons-react'
-import React, { PropTypes } from 'react'
+import React, { PropTypes, Component } from 'react'
 import { Router } from 'react-router'
 import routes from '../routes'
 // import AppBar from '../AppBar'
 // import Drawer from '../Drawer'
 // import Immutable from 'immutable'
 
-let Application = React.createClass({
-  propTypes: {
-    // currentRoute: PropTypes.instanceOf(Immutable.Map).isRequired,
-    // children: PropTypes.node.isRequired
-  },
+@provideContext
+class Application extends Component {
+  // propTypes: {
+  //   currentRoute: PropTypes.instanceOf(Immutable.Map).isRequired,
+  //   children: PropTypes.node.isRequired
+  // },
 
   render () {
-    // const { children } = this.props
+    const { context } = this.props
+
     return (
       <Router
         routes={routes}
-        onUpdate={function (...args) {
-          console.log('onUpdate', args)
+        onUpdate={function () {
+          // Has the router's context, shh don't tell anyone
+          const { branch, components, location, params } = this.state
+          context.executeAction(navigateAction, { branch, components, location, params })
+          // console.log('this.state', this.state)
         }}
         onError={() => console.log('onError')}
         onAbort={() => console.log('onAbort')}
@@ -28,8 +34,6 @@ let Application = React.createClass({
       />
     )
   }
-})
-
-Application = provideContext(Application)
+}
 
 export default Application
