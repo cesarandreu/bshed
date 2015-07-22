1a276051eb34be360b88f7f4f20d7944da037d87
Cesar Andreu <cesarandreu@gmail.com>
2015-07-22 03:13:34 -0700

    Add components

--- client/components/AppBar/AppBar.jsx
+++ client/components/AppBar/AppBar.jsx
@@ -1,16 +1,31 @@
 import './AppBar.less'
 
 import React from 'react'
-import { Link } from 'react-router'
-import BaseButton from '../common/BaseButton'
-import ActionMixin from '../../lib/ActionMixin'
-import * as SidebarActions from '../../actions/SidebarActions'
+// import { Link } from 'react-router'
+import BaseButton from '../BaseButton'
+// import * as SidebarActions from '../../actions/SidebarActions'
+
+// export const AppBarMenuButton = React.createClass({
+//   shouldComponentUpdate () {
+//     return false
+//   },
+
+//   render () {
+//     return (
+//       <BaseButton
+//         onClick={() => this.executeAction(SidebarActions.toggle)}
+//         className='app-bar-menu-button'
+//       >
+//         <img
+//           src={require('./ic_menu_white_24px.svg')}
+//           className='app-bar-menu-button-icon'
+//         />
+//       </BaseButton>
+//     )
+//   }
+// })
 
 export const AppBarMenuButton = React.createClass({
-  mixins: [
-    ActionMixin
-  ],
-
   shouldComponentUpdate () {
     return false
   },
@@ -18,7 +33,6 @@ export const AppBarMenuButton = React.createClass({
   render () {
     return (
       <BaseButton
-        onClick={() => this.executeAction(SidebarActions.toggle)}
         className='app-bar-menu-button'
       >
         <img
@@ -30,19 +44,19 @@ export const AppBarMenuButton = React.createClass({
   }
 })
 
-export const AppBarTitle = React.createClass({
-  shouldComponentUpdate () {
-    return false
-  },
+// export const AppBarTitle = React.createClass({
+//   shouldComponentUpdate () {
+//     return false
+//   },
 
-  render () {
-    return (
-      <Link to='home' className='app-bar-title'>
-        Bikeshed it!
-      </Link>
-    )
-  }
-})
+//   render () {
+//     return (
+//       <Link to='home' className='app-bar-title'>
+//         Bikeshed it!
+//       </Link>
+//     )
+//   }
+// })
 
 export const AppBar = React.createClass({
   shouldComponentUpdate () {
@@ -54,7 +68,9 @@ export const AppBar = React.createClass({
       <div className='app-bar-placeholder'>
         <nav className='app-bar'>
           <AppBarMenuButton/>
-          <AppBarTitle/>
+          <div className='app-bar-title'>
+            Bikeshed it!
+          </div>
         </nav>
       </div>
     )
