15bdbab1de557d2f7d22efa340c597e0ed2452cd
Cesar Andreu <cesarandreu@gmail.com>
2015-07-22 03:11:35 -0700

    Update bikeshed builder actions for redux

--- client/actions/SidebarActions.js
+++ client/actions/SidebarActions.js
@@ -3,13 +3,17 @@ import SidebarConstants from '../constants/SidebarConstants'
 /**
  * Sidebar toggle
  */
-export function toggle (context) {
-  context.dispatch(SidebarConstants.TOGGLE)
+export function toggle () {
+  return {
+    type: SidebarConstants.TOGGLE
+  }
 }
 
 /**
  * Sidebar close
  */
-export function close (context) {
-  context.dispatch(SidebarConstants.CLOSE)
+export function close () {
+  return {
+    type: SidebarConstants.CLOSE
+  }
 }
