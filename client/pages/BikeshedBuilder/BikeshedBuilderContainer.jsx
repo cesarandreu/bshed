e128dfde8bf788ccb7fa97d7c7f9170af3177498
Cesar Andreu <cesarandreu@gmail.com>
2015-07-25 03:14:19 -0700

    Update bikeshed builder container with redux-rx code

--- client/pages/BikeshedBuilder/BikeshedBuilderContainer.jsx
+++ client/pages/BikeshedBuilder/BikeshedBuilderContainer.jsx
@@ -1,25 +1,67 @@
-import * as BikeshedBuilderActions from '../../actions/BikeshedBuilderActions'
-import React, { Component, PropTypes } from 'react'
+import Rx from 'rx'
+import React from 'react'
+import { bindActionCreators } from 'redux-rx'
 import BikeshedBuilder from './BikeshedBuilder'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import { createConnector } from 'redux-rx/react'
+import { transitionTo } from 'redux-react-router'
+import * as BikeshedBuilderActions from '../../actions/BikeshedBuilderActions'
 
-@connect(({ bikeshedBuilder }) => ({
-  bikeshedBuilder
-}))
-export default class BikeshedBuilderContainer extends Component {
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    bikeshedBuilder: PropTypes.object.isRequired
+var observer = Rx.Observer.create(
+  function (x) {
+    console.log('Next: ' + x)
+  },
+  function (err) {
+    console.log('Error: ' + err)
+  },
+  function () {
+    console.log('Completed')
   }
+)
 
-  render () {
-    const { dispatch, bikeshedBuilder } = this.props
-    return (
-      <BikeshedBuilder
-        bikeshedBuilder={bikeshedBuilder}
-        { ...bindActionCreators(BikeshedBuilderActions, dispatch) }
-      />
+export default createConnector((props$, state$, dispatch$) => {
+  const bikeshedBuilder$ = state$.map(state => state.bikeshedBuilder)
+  const actionCreators$ = bindActionCreators(BikeshedBuilderActions, dispatch$)
+  const transitionTo$ = bindActionCreators({ transitionTo }, dispatch$).map(ac => ac.transitionTo)
+  const reset$ = actionCreators$.map(ac => ac.reset)
+
+  // Detect navigation to and from bikeshed builder and create reset action
+  const bikeshedBuilderNavigation$ = state$
+    .distinctUntilChanged(state => state.router.pathname === '/')
+    .withLatestFrom(reset$, (state, reset) => () => reset())
+    .do(go => go())
+
+  bikeshedBuilder$.subscribe(observer)
+
+  const createdBikeshed$ = bikeshedBuilder$
+    .map(bikeshedBuilder => bikeshedBuilder.get('createdBikeshed'))
+    .distinctUntilChanged(createdBikeshed => createdBikeshed)
+    .filter(createdBikeshed => createdBikeshed)
+
+  createdBikeshed$.subscribe(observer)
+
+  const transitionToNewBikeshed$ = createdBikeshed$
+    .withLatestFrom(transitionTo$, (createdBikeshed, transitionTo) =>
+      // () => transitionTo(`/bikesheds/${createdBikeshed}`)
+      () => transitionTo(`/about`)
     )
-  }
-}
+    .do(go => go())
+
+  transitionToNewBikeshed$.subscribe(observer)
+
+  return Rx.Observable.combineLatest(
+    bikeshedBuilder$,
+    actionCreators$,
+    bikeshedBuilderNavigation$,
+    // transitionToNewBikeshed$,
+    // bikeshedBuilderNavigation$,
+    // bikeshedBuilderSuccess$,
+    (bikeshedBuilder, actionCreators) => {
+      return {
+        ...actionCreators,
+        ...{ bikeshedBuilder }
+      }
+    }
+  )
+}, props => {
+  return <BikeshedBuilder { ...props }/>
+})
