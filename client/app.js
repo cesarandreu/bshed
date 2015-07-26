a83cce1daa2773283bf34789c8d2907b4348c07a
Cesar Andreu <cesarandreu@gmail.com>
2015-07-26 05:20:55 -0700

    Update app entry-point

--- client/app.js
+++ client/app.js
@@ -1,35 +1,13 @@
-// // Styles
-// import './components/styles/base.less'
 /**
  * Application entry-point
  * @flow
  */
 
-// // Modules
-// import Fluxible from 'fluxible'
+// Styles
+import './components/styles/base.less'
 
-// // Plugins
-// import RequestPlugin from './lib/RequestPlugin'
+// Reducers
+export * as reducers from './reducers'
 
-// // Routes
-// // import routes from './components/routes.jsx'
-// import Application from './components/Application'
-
-// // Stores
-// // import ApplicationStore from './stores/ApplicationStore'
-// import BikeshedBuilderStore from './stores/BikeshedBuilderStore'
-// import SidebarStore from './stores/SidebarStore'
-
-// // Initialize application
-// const app = new Fluxible({
-//   component: Application,
-//   stores: [
-//     // ApplicationStore,
-//     BikeshedBuilderStore,
-//     SidebarStore
-//   ]
-// })
-
-// app.plug(RequestPlugin())
-
-// export default app
+// Routes
+export { routes } from './pages'
