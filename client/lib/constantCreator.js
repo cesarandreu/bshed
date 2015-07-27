02464efee77146e404ce82233e626277ff26425a
Cesar Andreu <cesarandreu@gmail.com>
2015-07-26 19:28:47 -0700

    Remove useless default value from constant creator

--- client/lib/constantCreator.js
+++ client/lib/constantCreator.js
@@ -5,11 +5,11 @@
 
 /**
  * Like keymirror, but adds name as a value prefix
- * @param {string} [name=''] Constant name prefix
+ * @param {string} name Constant name prefix
  * @param {Object} constants Object of constants
  * @returns {Object} Constants object
  */
-export default function constantCreator (name='', constants: Object) {
+export default function constantCreator (name: string, constants: Object) {
   name = name.toUpperCase()
 
   return Object.keys(constants).reduce((constants, constant) => {
