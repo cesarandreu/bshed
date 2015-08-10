0e71a4a808a7526fdcbb434349c73e1d068dc300
Cesar Andreu <cesarandreu@gmail.com>
2015-08-09 19:54:22 -0700

    Add BikeshedBuilder page

---
+++ client/pages/BikeshedBuilder/BikeshedBuilderConstants.js
@@ -0,0 +1,13 @@
+/**
+ * BikeshedBuilder constants
+ * @flow
+ */
+import createConstants from '../../lib/createConstants'
+
+export default createConstants('BIKESHED_BUILDER', {
+  RESET: null
+})
+
+export const IMAGE_SIZE = 128
+export const MAXIMUM_IMAGE_COUNT = 5
+export const MINIMUM_IMAGE_COUNT = 2
---
+++ client/pages/BikeshedBuilder/index.js
@@ -0,0 +1,10 @@
+/**
+ * BikeshedBuilder page
+ * @flow
+ */
+export BikeshedBuilder from './BikeshedBuilder'
+export BikeshedBuilderPage from './BikeshedBuilderPage'
+export BikeshedBuilderReducer from './BikeshedBuilderReducer'
+export BikeshedBuilderContainer from './BikeshedBuilderContainer'
+export * as BikeshedBuilderActions from './BikeshedBuilderActions'
+export * as BikeshedBuilderConstants from './BikeshedBuilderConstants'
