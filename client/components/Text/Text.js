d475467c2db2155a24186a06af8700e0239d10ad
Cesar Andreu <cesarandreu@gmail.com>
2015-09-14 01:32:17 -0700

    Use plain functions instead of classes for Text components

--- client/components/Text/Text.js
+++ client/components/Text/Text.js
@@ -1,105 +1,89 @@
-import React, { Component, PropTypes } from 'react'
+/**
+ * Text components
+ * @flow
+ */
+import React, { PropTypes } from 'react'
 import textClassNames from './Text.css'
 import cn from 'classnames'
 
-export class Text extends Component {
-  static propTypes = {
-    className: PropTypes.string
-  }
-
-  render () {
-    const { className, ...props } = this.props
-    return (
-      <p
-        className={cn(className, textClassNames.text)}
-        {...props}
-      />
-    )
-  }
-}
+/**
+ * Text
+ */
 export default Text
-
-export class Subhead extends Component {
-  static propTypes = {
-    className: PropTypes.string
-  }
-
-  render () {
-    const { className, ...props } = this.props
-    return (
-      <h6
-        className={cn(className, textClassNames.subhead)}
-        {...props}
-      />
-    )
-  }
+export function Text ({ className, ...props }) {
+  return (
+    <p
+      className={cn(className, textClassNames.text)}
+      {...props}
+    />
+  )
+}
+Text.propTypes = {
+  className: PropTypes.string
 }
 
-export class Title extends Component {
-  static propTypes = {
-    className: PropTypes.string
-  }
-
-  render () {
-    const { className, ...props } = this.props
-    return (
-      <h5
-        className={cn(className, textClassNames.title)}
-        {...props}
-      />
-    )
-  }
+/**
+ * Subhead
+ */
+export function Subhead ({ className, ...props }) {
+  return (
+    <h6
+      className={cn(className, textClassNames.subhead)}
+      {...props}
+    />
+  )
+}
+Subhead.propTypes = {
+  className: PropTypes.string
 }
 
-export class Headline extends Component {
-  static propTypes = {
-    className: PropTypes.string
-  }
-
-  render () {
-    const { className, ...props } = this.props
-    return (
-      <h4
-        className={cn(className, textClassNames.headline)}
-        {...props}
-      />
-    )
-  }
+/**
+ * Title
+ */
+export function Title ({ className, ...props }) {
+  return (
+    <h5
+      className={cn(className, textClassNames.title)}
+      {...props}
+    />
+  )
+}
+Title.propTypes = {
+  className: PropTypes.string
 }
 
-export class Display extends Component {
-  static propTypes = {
-    variation: PropTypes.oneOf([1, 2, 3]).isRequired,
-    className: PropTypes.string
-  }
+/**
+ * Headline
+ */
+export function Headline ({ className, ...props }) {
+  return (
+    <h4
+      className={cn(className, textClassNames.headline)}
+      {...props}
+    />
+  )
+}
+Headline.propTypes = {
+  className: PropTypes.string
+}
 
-  static defaultProps = {
-    variation: 1
+/**
+ * Display
+ */
+export function Display ({ variation, className, ...props }) {
+  switch (variation) {
+    case 1:
+      return <h3 className={cn(textClassNames.display1, className)} {...props}/>
+    case 2:
+      return <h2 className={cn(textClassNames.display2, className)} {...props}/>
+    default:
+      return <h1 className={cn(textClassNames.display3, className)} {...props}/>
   }
-
-  render () {
-    const { variation, className, ...props } = this.props
-    if (variation === 1) {
-      return (
-        <h3
-          className={cn(textClassNames.display1, className)}
-          {...props}
-        />
-      )
-    } else if (variation === 2) {
-      return (
-        <h2
-          className={cn(textClassNames.display2, className)}
-          {...props}
-        />
-      )
-    } else {
-      return (
-        <h1
-          className={cn(textClassNames.display3, className)}
-          {...props}
-        />
-      )
-    }
-  }
+}
+Display.propTypes = {
+  variation: PropTypes.oneOf([1, 2, 3]).isRequired,
+  className: PropTypes.string
+}
+Display.defaultProps = {
+  variation: 1
 }
