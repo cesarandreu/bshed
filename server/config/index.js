5c3c8afcbeae2f760d742c05e07c65a32f88cc0b
Cesar Andreu <cesarandreu@gmail.com>
2015-09-13 21:27:34 -0700

    Fix server config

--- server/config/index.js
+++ server/config/index.js
@@ -2,8 +2,10 @@
  * Application configuration
  * @flow
  */
-import * as db from './database'
+import * as databaseConfig from './database'
+import * as awsConfig from './aws'
 
 export const env = process.env.NODE_ENV || 'development'
 export const port = Number(process.env.PORT) || 3000
-export const database = db[env]
+export const database = databaseConfig[env]
+export const aws = awsConfig[env]
