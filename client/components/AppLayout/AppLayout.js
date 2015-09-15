1de51c2c8927763add1d9171fd9d5bd4ce223a07
Cesar Andreu <cesarandreu@gmail.com>
2015-09-14 21:28:04 -0700

    Add AppLayout

---
+++ client/components/AppLayout/AppLayout.js
@@ -0,0 +1,33 @@
+import React, { Component, PropTypes } from 'react'
+import { Layout, LayoutContent, LayoutHeader } from '../Layout'
+
+export default class AppLayout extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired
+  }
+
+  render () {
+    const { children } = this.props
+
+    return (
+      <Layout>
+        <LayoutHeader>
+          HEADER~
+        </LayoutHeader>
+        <LayoutContent>
+          {children}
+        </LayoutContent>
+      </Layout>
+    )
+  }
+}
+
+// export default Relay.createContainer(Layout, {
+//   fragments: {
+//     viewer: () => Relay.QL`
+//       fragment on Viewer {
+//         isRegistered,
+//       }
+//     `
+//   }
+// })
