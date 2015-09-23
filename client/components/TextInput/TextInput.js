3ea9e9740ec3ddcbd7186392b3dde9031c911a7e
Cesar Andreu <cesarandreu@gmail.com>
2015-09-22 18:40:15 -0700

    Add TextInput component

---
+++ client/components/TextInput/TextInput.js
@@ -0,0 +1,83 @@
+/**
+ * TextInput
+ * @flow
+ */
+import shouldPureComponentUpdate from 'react-pure-render/function'
+import React, { Component, PropTypes } from 'react'
+import textInputClassNames from './TextInput.css'
+import type SyntheticEvent from 'react'
+import cn from 'classnames'
+
+export class TextInput extends Component {
+  constructor (props: Object) {
+    super(props)
+    this.state = { focus: false }
+    this._onBlur = this._onBlur.bind(this)
+    this._onFocus = this._onFocus.bind(this)
+    this._onKeyDown = this._onKeyDown.bind(this)
+    this.shouldComponentUpdate = shouldPureComponentUpdate
+  }
+
+  _onFocus () {
+    this.setState({ focus: true })
+  }
+
+  _onBlur () {
+    this.setState({ focus: false })
+  }
+
+  _onKeyDown (e: SyntheticEvent) {
+    if (e.key === 'Escape') {
+      this.refs.input.blur()
+    }
+  }
+
+  render () {
+    const { name, error, label, value, onChange } = this.props
+    const { focus } = this.state
+
+    const labelClassNames = cn(textInputClassNames.label, {
+      [textInputClassNames.hasFocus]: !!focus,
+      [textInputClassNames.hasError]: !!error,
+      [textInputClassNames.hasValue]: !!value
+    })
+
+    const focusUnderlineClassNames = cn(textInputClassNames.focusUnderline, {
+      [textInputClassNames.hasFocus]: !!focus,
+      [textInputClassNames.hasError]: !!error
+    })
+
+    return (
+      <div className={textInputClassNames.wrapper}>
+        <label
+          className={labelClassNames}
+          htmlFor={name}
+        >
+          {label || name}
+        </label>
+        <input
+          ref='input'
+          type='text'
+          name={name}
+          value={value}
+          autoComplete='off'
+          onChange={onChange}
+          onBlur={this._onBlur}
+          onFocus={this._onFocus}
+          onKeyDown={this._onKeyDown}
+          className={textInputClassNames.input}
+        />
+        <hr className={textInputClassNames.underline}/>
+        <hr className={focusUnderlineClassNames}/>
+        {error ? <div className={textInputClassNames.error}>{error}</div> : null}
+      </div>
+    )
+  }
+}
+TextInput.propTypes = {
+  label: PropTypes.string,
+  error: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+}
