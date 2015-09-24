ceefdfb4cf97467eff599d7ad5caabcaf2f6ba66
Cesar Andreu <cesarandreu@gmail.com>
2015-09-23 20:19:30 -0700

    Workaround for flow types

---
+++ interfaces/interfaces.js
@@ -0,0 +1,40 @@
+/* @flow */
+declare module CSSModule {
+  declare var exports: { [key: string]: string };
+}
+
+declare module 'CSSModule' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module '../styles/resets.css' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module './BikeshedCreator.css' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module './Button.css' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module './Card.css' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module './Grid.css' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module './Layout.css' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module './Text.css' {
+  declare var exports: { [key: string]: string };
+}
+
+declare module './TextInput.css' {
+  declare var exports: { [key: string]: string };
+}
