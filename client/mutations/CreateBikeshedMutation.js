4cb2d74673e462e5b2c8499f92b088378a751032
Cesar Andreu <cesarandreu@gmail.com>
2015-09-26 16:21:29 -0700

    Add CreateBikeshedMutation

---
+++ client/mutations/CreateBikeshedMutation.js
@@ -0,0 +1,65 @@
+/**
+ * Create Bikeshed Mutation
+ * @flow
+ */
+import Relay from 'react-relay'
+const { File } = global
+
+export class CreateBikeshedMutation extends Relay.Mutation {
+  getMutation () {
+    return Relay.QL`mutation{createBikeshed}`
+  }
+
+  getVariables (): { description: string } {
+    const { description } = this.props
+    return {
+      description
+    }
+  }
+
+  getFiles (): { [key: string]: File } {
+    const { files } = this.props
+    return files.reduce((fileMap, file, idx) => {
+      fileMap[`images${idx}`] = file
+      return fileMap
+    }, {})
+  }
+
+  getFatQuery () {
+    return Relay.QL`
+      fragment on CreateBikeshedPayload {
+        bikeshedEdge,
+        viewer {
+          bikesheds
+        }
+      }
+    `
+  }
+
+  getConfigs () {
+    return [{
+      type: 'RANGE_ADD',
+      parentName: 'viewer',
+      parentID: this.props.viewer.id,
+      connectionName: 'bikesheds',
+      edgeName: 'bikeshedEdge',
+      rangeBehaviors: {
+        '': 'append'
+      }
+    }]
+  }
+
+  getOptimisticResponse () {
+
+  }
+}
+
+CreateBikeshedMutation.fragments = {
+  viewer () {
+    return Relay.QL`
+      fragment on User {
+        id
+      }
+    `
+  }
+}
