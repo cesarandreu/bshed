45b5a1a5f7ea59a38e7e9a592643fb1eaf04e205
Cesar Andreu <cesarandreu@gmail.com>
2015-09-26 16:15:39 -0700

    Remove unused code

--- server/lib/s3.js
+++ server/lib/s3.js
@@ -8,21 +8,3 @@ import { S3 } from 'aws-sdk'
 export default function s3Loader (config: Object): S3 {
   return new S3(config)
 }
-
-// Upload a file to S3
-export function uploadFile (client: S3, uploadConfig: Object): Promise {
-  return new Promise((resolve, reject) => {
-    client.upload(uploadConfig, (err, data) => {
-      err ? reject(err) : resolve(data)
-    })
-  })
-}
-
-// Delete an object from S3
-export function deleteObject (client: S3, deleteConfig: Object): Promise {
-  return new Promise((resolve, reject) => {
-    client.deleteObject(deleteConfig, (err, data) => {
-      err ? reject(err) : resolve(data)
-    })
-  })
-}
