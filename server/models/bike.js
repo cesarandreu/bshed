f2eb7cc7fb48920b1557890d1648da041149a24f
Cesar Andreu <cesarandreu@gmail.com>
2015-09-26 16:14:12 -0700

    Comment out bike size fields

--- server/models/bike.js
+++ server/models/bike.js
@@ -2,10 +2,6 @@
  * Bike
  * @flow
  */
-import fs from 'fs'
-import assert from 'assert'
-import getImageDimensions from '../lib/getImageDimensions'
-
 export default function createBike (sequelize, DataTypes) {
   const Bike = sequelize.define('Bike', {
     id: {
@@ -20,14 +16,14 @@ export default function createBike (sequelize, DataTypes) {
         notEmpty: true
       }
     },
-    width: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    height: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
+    // width: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false
+    // },
+    // height: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: false
+    // },
     size: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -55,74 +51,6 @@ export default function createBike (sequelize, DataTypes) {
       associate (models: Object) {
         Bike.Bikeshed = Bike.belongsTo(models.Bikeshed)
         Bike.Ratings = Bike.hasMany(models.Rating)
-      },
-
-      /**
-       * Create bike and upload image to s3
-       * @param {Object} config
-       * @param {string} config.BikeshedId
-       * @param {Object} config.file
-       * @param {Object} options
-       * @param {Object} options.s3
-       * @param {Object} [options.transaction]
-       * @returns {Promise} Bike creation and upload promise
-       */
-      async createAndUpload ({ BikeshedId, file } = {}, { s3, transaction } = {}) {
-        assert(BikeshedId && file, 'createAndUpload requires BikeshedId and file')
-        assert(s3, 'createAndUpload requires s3')
-
-        const {width, height} = await getImageDimensions(file.path)
-        const {type, size, name} = file
-
-        const bike = await Bike.create({
-          BikeshedId,
-          height,
-          width,
-          type,
-          size,
-          name
-        }, {
-          transaction
-        })
-
-        await Bike.uploadImage(s3, {
-          path: file.path,
-          BikeId: bike.id,
-          BikeshedId
-        })
-
-        return bike
-      },
-
-      /**
-       * Upload image to s3
-       * @param {Object} s3 Client instance of s3
-       * @param {Object} config
-       * @param {string} config.BikeId Bike UUID
-       * @param {string} config.BikeshedId Bikeshed UUID
-       * @param {string} config.path Image location
-       * @returns {Promise} Upload image promise
-       */
-      async uploadImage (s3, { BikeshedId, BikeId, path } = {}) {
-        assert(s3, 'uploadBike requires s3')
-        assert(BikeshedId && BikeId && path, 'uploadImage requires BikeshedId, BikeId, and path')
-
-        const Key = `${BikeshedId}/${BikeId}`
-        const Bucket = 'bshed'
-
-        const uploadFileOptions = {
-          Body: fs.createReadStream(path),
-          ACL: 'public-read',
-          Bucket,
-          Key
-        }
-
-        try {
-          await s3.uploadFilePromise(uploadFileOptions)
-        } catch (err) {
-          await s3.deleteObjectPromise({Bucket, Key})
-          throw err
-        }
       }
     }
   })
