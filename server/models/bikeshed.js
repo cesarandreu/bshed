23b68b7251a1994063bc97fba34935f86b305595
Cesar Andreu <cesarandreu@gmail.com>
2015-10-04 05:58:58 -0700

    Add user and bikeshed schema

--- server/models/bikeshed.js
+++ server/models/bikeshed.js
@@ -1,21 +1,41 @@
 /**
  * Bikeshed model
- * Schema:
- *  id
- *  description
- *  createdAt
- *  userId
  */
+import Joi from 'joi'
+import * as utils from './utils'
+
 export const INDEXES = ['userId', 'createdAt']
 export const TABLE = 'bikesheds'
 export const TYPE = 'Bikeshed'
 
-export async function create (r, values = {}) {
-  const { userId, description = '' } = values
-  const { generated_keys: [id] } = await r.table(TABLE).insert({
+export const FileListItemSchema = Joi.object().keys({
+  fieldname: Joi.string(),
+  originalname: Joi.string(),
+  encoding: Joi.string(),
+  mimetype: Joi.string(),
+  size: Joi.number(),
+  key: Joi.string()
+})
+
+export const BikeshedSchema = Joi.object().keys({
+  createdAt: Joi.date(),
+  id: Joi.string().guid(),
+  userId: Joi.string().guid(),
+  requestId: Joi.string().guid(),
+  fileList: Joi.array().min(2).max(8).items(FileListItemSchema),
+  description: Joi.string().max(4096).allow('').default('').optional()
+})
+
+export const validate = utils.createValidate(BikeshedSchema)
+
+export const get = utils.createGet(TABLE)
+
+export async function create (r, values) {
+  const bikeshedValues = validate(values)
+
+  const { generated_keys: [bikeshedId] } = await r.table(TABLE).insert({
     createdAt: r.now(),
-    description,
-    userId
+    ...bikeshedValues
   })
-  return id
+  return bikeshedId
 }
--- server/models/user.js
+++ server/models/user.js
@@ -1,18 +1,30 @@
 /**
  * User model
- * Schema:
- *  id
- *  name
- *  email
- *  createdAt
  */
+import Joi from 'joi'
+import * as utils from './utils'
+
 export const INDEXES = []
 export const TABLE = 'users'
 export const TYPE = 'User'
 
-export async function create (r, values = {}) {
-  const { generated_keys: [id] } = await r.table(TABLE).insert({
-    createdAt: r.now()
+export const UserSchema = Joi.object().keys({
+  createdAt: Joi.date(),
+  id: Joi.string().guid(),
+  name: Joi.string().max(256).default('').allow(''),
+  email: Joi.string().email().default('').allow('')
+})
+
+export const validate = utils.createValidate(UserSchema)
+
+export const get = utils.createGet(TABLE)
+
+export async function create (r, values) {
+  const userValues = validate(values)
+
+  const { generated_keys: [userId] } = await r.table(TABLE).insert({
+    createdAt: r.now(),
+    ...userValues
   })
-  return id
+  return userId
 }
