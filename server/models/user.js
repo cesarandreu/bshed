52338066b692ff6ca04a958c9b36764e90701261
Cesar Andreu <cesarandreu@gmail.com>
2015-10-11 09:59:43 -0700

    Update User to use BaseModel

--- server/models/user.js
+++ server/models/user.js
@@ -2,29 +2,23 @@
  * User model
  */
 import Joi from 'joi'
-import * as utils from './utils'
+import BaseModel from './model'
 
-export const INDEXES = []
-export const TABLE = 'users'
-export const TYPE = 'User'
-
-export const UserSchema = Joi.object().keys({
+const UserSchema = Joi.object()
+.keys({
   createdAt: Joi.date(),
   id: Joi.string().guid(),
   name: Joi.string().max(256).default('').allow(''),
   email: Joi.string().email().default('').allow('')
 })
 
-export const validate = utils.createValidate(UserSchema)
-
-export const get = utils.createGet(TABLE)
-
-export async function create (r, values) {
-  const userValues = validate(values)
-
-  const { generated_keys: [userId] } = await r.table(TABLE).insert({
-    createdAt: r.now(),
-    ...userValues
-  })
-  return userId
+const PROPS = {
+  SCHEMA: UserSchema,
+  INDEXES: [],
+  TABLE: 'users',
+  TYPE: 'User'
 }
+
+export default class User extends BaseModel {
+}
+Object.assign(User.prototype, PROPS)
