3859c7f6c76a9c5c81a6cb97c608854e4a42a9db
Cesar Andreu <cesarandreu@gmail.com>
2015-10-11 10:00:51 -0700

    Update models index to work with new models

--- server/models/index.js
+++ server/models/index.js
@@ -1,40 +1,44 @@
 /**
  * Model loader
- * Each model is expected to export the following:
+ * Each model is expected to have the following:
  *  TYPE string
  *  TABLE string
  *  INDEXES array of strings
+ *  SCHEMA joi schema definition
  */
-import debug from 'debug'
 import RethinkDB from 'rethinkdbdash'
+import invariant from 'invariant'
+import BaseModel from './model'
+import debug from 'debug'
+
 const log = debug('app:models')
 
 // Model imports
-import * as Bikeshed from './bikeshed'
-import * as User from './user'
+import Bikeshed from './bikeshed'
+import User from './user'
 
 const models = {
   Bikeshed,
   User
 }
 
-// Get a list with each model's name, table, and secondary indexes
-export function getModelList () {
-  return Object.keys(models).map(modelName => {
-    const { TYPE, TABLE, INDEXES } = models[modelName]
-    return {
-      TYPE,
-      TABLE,
-      INDEXES
-    }
-  })
+export function instantiateModels (r) {
+  return Object.entries(models).reduce((instances, [name, Model]) => {
+    invariant(
+      BaseModel.isPrototypeOf(Model),
+      `Models: Expected ${Model} to extend BaseModel.`
+    )
+
+    instances[name] = new Model({ r })
+    return instances
+  }, {})
 }
 
 // Load database and models
 export default function modelLoader (config: Object): Object {
   log('start')
   const r = RethinkDB(config)
-
+  const instances = instantiateModels(r)
   log('end')
-  return { r, ...models }
+  return { r, models, ...instances }
 }
