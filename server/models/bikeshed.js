6e3de081629cc7150f095deaa228e69133935a29
Cesar Andreu <cesarandreu@gmail.com>
2015-10-19 21:29:51 -0700

    Add updateStatus to bikeshed model

--- server/models/bikeshed.js
+++ server/models/bikeshed.js
@@ -20,8 +20,16 @@ const BikeshedSchema = Joi.object()
   id: Joi.string().guid(),
   userId: Joi.string().guid().required(),
   requestId: Joi.string().guid().required(),
+  info: Joi.string().allow('').default('').optional(),
   description: Joi.string().max(4096).allow('').default('').optional(),
-  fileList: Joi.array().min(2).max(8).items(FileListItemSchema).required()
+  fileList: Joi.array().min(2).max(8).items(FileListItemSchema).required(),
+  status: Joi.string()
+    .default('queued')
+    .only('queued')
+    .only('processing')
+    .only('error')
+    .only('ready')
+    .optional()
 })
 .requiredKeys(['userId', 'requestId', 'fileList'])
 
@@ -33,5 +41,23 @@ const PROPS = {
 }
 
 export default class Bikeshed extends BaseModel {
+  async updateStatus (bikeshedId: string, nextStatus: string, payload: any) {
+    const resource = `locks:bikeshed:${bikeshedId}`
+    const lock = await this.redlock.lock(resource, 1e3)
+    try {
+      const bikeshed = await this.get(bikeshedId)
+      const nextFields = { status: nextStatus }
+      if (nextStatus === 'ready' && Array.isArray(payload)) {
+        nextFields.fileList = bikeshed.fileList.map((file, idx) =>
+          ({ ...file, ...payload[idx] })
+        )
+      } else if (nextStatus === 'error' && payload) {
+        nextFields.info = payload
+      }
+      await this.get(bikeshedId).update(nextFields)
+    } finally {
+      await lock.unlock()
+    }
+  }
 }
 Object.assign(Bikeshed.prototype, PROPS)
