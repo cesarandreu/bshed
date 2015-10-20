1fb29ec7d0139ef133aae56922402dc15ebb3de5
Cesar Andreu <cesarandreu@gmail.com>
2015-10-19 21:28:23 -0700

    Update server config

--- server/config/index.js
+++ server/config/index.js
@@ -2,12 +2,12 @@
  * Configuration loader
  */
 import * as databaseConfig from './database'
-import * as queueConfig from './queue'
+import * as redisConfig from './redis'
 import * as awsConfig from './aws'
 
 export const env = process.env.NODE_ENV || 'development'
 export const port = Number(process.env.PORT) || 3000
 export const database = databaseConfig[env]
-export const queue = queueConfig[env]
+export const redis = redisConfig[env]
 export const aws = awsConfig[env]
 export const keys = ['keys']
