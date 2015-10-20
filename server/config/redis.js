bdf12cbb4eb5c56a70ef635c6162bb3ea0ff3e02
Cesar Andreu <cesarandreu@gmail.com>
2015-10-19 21:21:26 -0700

    Replace kue config with redis config

--- server/config/redis.js
+++ server/config/redis.js
@@ -6,12 +6,8 @@ export const production = {
 }
 
 export const development = {
-  jobEvents: false,
-  prefix: 'q',
-  redis: {
-    port: 6379,
-    host: '127.0.0.1'
-  }
+  port: 6379,
+  host: '127.0.0.1'
 }
 
 export const test = {
