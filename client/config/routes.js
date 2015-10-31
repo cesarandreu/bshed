a4d2fd544cc9d933ee586fdec59fa1fe6482a5d9
Cesar Andreu <cesarandreu@gmail.com>
2015-10-31 03:34:04 -0700

    Update routes

--- client/config/routes.js
+++ client/config/routes.js
@@ -6,11 +6,14 @@ import React from 'react'
 import { IndexRoute, Route } from 'react-router'
 
 // Queries
-import ViewerQueries from '../queries/ViewerQueries'
+import ViewerQueries from '@client/queries/ViewerQueries'
+import BikeshedQueries from '@client/queries/BikeshedQueries'
 
 // Components
-import { AppContainer } from '../components/App'
-import { BikeshedCreatorContainer } from '../components/BikeshedCreator'
+import { AppContainer } from '@components/App'
+import { BikeshedCreatorContainer } from '@components/BikeshedCreator'
+import { BikeshedExplorerContainer } from '@components/BikeshedExplorer'
+import { BikeshedViewerContainer } from '@components/BikeshedViewer'
 
 export default (
   <Route
@@ -22,5 +25,18 @@ export default (
       component={BikeshedCreatorContainer}
       queries={ViewerQueries}
     />
+
+    <Route
+      path='/bikesheds'
+    >
+      <IndexRoute
+        component={BikeshedExplorerContainer}
+      />
+      <Route
+        path=':bikeshedId'
+        component={BikeshedViewerContainer}
+        queries={BikeshedQueries}
+      />
+    </Route>
   </Route>
 )
