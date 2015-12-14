2322b8a6e0aec5e6f4f5bce20d581fa971dc5948
Cesar Andreu <cesarandreu@gmail.com>
2015-12-14 02:16:40 -0800

    Add Text components

---
+++ client/components/Text/Text.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styles from './Text.css'
+
+export function Headline ({ children }) {
+  return (
+    <h3 className={styles.headline}>
+      {children}
+    </h3>
+  )
+}
+
+export function Hint ({ children }) {
+  return (
+    <div className={styles.hint}>
+      {children}
+    </div>
+  )
+}
+
+export function PrimaryText ({ children }) {
+  return (
+    <div className={styles.primary}>
+      {children}
+    </div>
+  )
+}
+
+export function SecondaryText ({ children }) {
+  return (
+    <div className={styles.secondary}>
+      {children}
+    </div>
+  )
+}
+
+export function Title ({ children }) {
+  return (
+    <div className={styles.title}>
+      {children}
+    </div>
+  )
+}
---
+++ client/components/Text/index.js
@@ -0,0 +1,7 @@
+export {
+  Headline,
+  Hint,
+  PrimaryText,
+  SecondaryText,
+  Title
+} from './Text'
