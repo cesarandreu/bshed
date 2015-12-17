da770e1e0a21ac678c006f0d646e00ea6630d70b
Cesar Andreu <cesarandreu@gmail.com>
2015-12-17 00:34:13 -0800

    Clean up Card

--- client/components/Card/Card.js
+++ client/components/Card/Card.js
@@ -1,12 +1,14 @@
+// @TODO: Add more elevations if needed
 import { Headline } from 'components/Text'
 import React, { PropTypes } from 'react'
 import styles from './Card.css'
+import cn from 'classnames'
 
 export function Card ({ children, elevation, heading }) {
   return (
     <div
       aria-label={heading}
-      className={`${styles.card} ${styles[`elevation${elevation}`]}`}
+      className={cn(styles.card, styles[`elevation${elevation}`])}
     >
       {heading != null && (
         <CardHeader>
@@ -18,15 +20,14 @@ export function Card ({ children, elevation, heading }) {
   )
 }
 
-// @TODO: Add more elevations if needed
 Card.propTypes = {
   children: PropTypes.node.isRequired,
-  elevation: PropTypes.oneOf([2]).isRequired,
+  elevation: PropTypes.oneOf([1]).isRequired,
   heading: PropTypes.string
 }
 
 Card.defaultProps = {
-  elevation: 2
+  elevation: 1
 }
 
 export function CardHeader ({ children }) {
