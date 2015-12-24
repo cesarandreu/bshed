53f54c4a54488a402c952f1fb15b24b652912211
Cesar Andreu <cesarandreu@gmail.com>
2015-12-24 01:23:22 -0800

    Rewrite graphql middleware

--- server/lib/graphql.js
+++ server/lib/graphql.js
@@ -1,137 +1,183 @@
-/**
- * GraphQL Controller
- */
+import bodyParser from 'co-body'
+import invariant from 'invariant'
+import createError from 'http-errors'
+import { execute } from 'graphql/execution'
 import { formatError } from 'graphql/error'
-import { graphql } from 'graphql'
-import Router from 'koa-router'
-import parse from 'co-body'
+import { validate } from 'graphql/validation'
+import { parse, Source } from 'graphql/language'
+import { getOperationAST } from 'graphql/utilities/getOperationAST'
 
 // Static parser options
 const PARSE_OPTIONS = { limit: '100kb' }
 
 /**
- * GraphQL controller
+ * GraphQL middleware
  */
-export default function GraphqlController () {
-  const routes = new Router()
-    .get(
-      '/graphql',
-      GraphqlController.handleErrors,
-      GraphqlController.prepare,
-      GraphqlController.perform
-    )
-    .post(
-      '/graphql',
-      GraphqlController.handleErrors,
-      GraphqlController.prepare,
-      GraphqlController.perform
-    )
+export default function graphqlHTTP (options) {
+  invariant(options, 'GraphQL middleware requires options param')
 
-  return routes.middleware()
-}
-
-// Wrapper for error-handling
-GraphqlController.handleErrors = function * handleErrors (next) {
-  try {
-    yield next
-  } catch (error) {
-    this.status = error.status || 500
-    this.body = { errors: [ formatError(error) ] }
+  return async function graphqlHTTPMiddleware (ctx) {
+    try {
+      return await perform(ctx, options)
+    } catch (error) {
+      ctx.status = error.status || 500
+      ctx.body = { errors: [ formatError(error) ] }
+    }
   }
 }
 
-// Prepare everything for GraphQL
-GraphqlController.prepare = function * prepare (next) {
-  // Make sure configuration is correct
-  const { schema, rootValue } = this.graphql
-  this.assert(schema, 500, 'GraphQL requires a schema.')
-  this.assert(rootValue, 500, 'GraphQL requires a rootValue.')
-
-  // Skip requests without content type
-  const hasContentType = this.request.type !== undefined
-
-  // If koa already has a body, use it
-  const hasEmptyBody = this.request.body === undefined
-
-  if (hasEmptyBody && hasContentType) {
-    // Use the correct body parser based on Content-Type header
-    switch (this.request.type) {
-      case 'application/graphql':
-        const body = yield parse.text(this, PARSE_OPTIONS)
-        this.request.body = { query: body }
-        break
-      case 'application/json':
-        this.request.body = yield parse.json(this, PARSE_OPTIONS)
-        break
-      case 'application/x-www-form-urlencoded':
-        this.request.body = yield parse.form(this, PARSE_OPTIONS)
-        break
-      case 'multipart/form-data':
-        yield this.uploader(this.req, this.res)
-        this.request.body = this.req.body
-
-        // Terrible hack
-        // Sample output: { image0: fileInfo, image1: fileInfo }
-        this.request.files = Object.entries(this.req.files)
-        .reduce((files, [name, [file]]) => {
-          files[name] = file
-          return files
-        }, {})
-        break
-      default:
-        this.request.body = {}
-        break
-    }
+/**
+ * Call each helper and make sure everything is going as expected
+ */
+async function perform (ctx, options) {
+  // @TODO: Remove? Not needed since it's handled upstream
+  if (ctx.method !== 'GET' && ctx.method !== 'POST') {
+    ctx.set('Allow', 'GET, POST')
+    ctx.throw(405, 'GraphQL only supports GET and POST requests.')
   }
 
-  // GraphQL query string
-  this.graphql.query = this.query.query || this.request.body.query
-  this.assert(this.graphql.query, 400, 'Must provide query string.')
+  // Get schema and rootValue
+  const { schema, rootValue } = await getOptions(options, ctx)
 
-  // Parse the variables if needed
-  const variables = this.query.variables || this.request.body.variables
-  if (variables) {
-    if (typeof variables === 'string') {
-      try {
-        this.graphql.variables = JSON.parse(variables)
-      } catch (err) {
-        this.throw(400, 'Variables are invalid JSON.')
-      }
-    } else {
-      this.graphql.variables = variables
-    }
-  }
+  // Parse the request body
+  const body = await parseBody(ctx)
 
-  // Name of GraphQL operation to execute
-  this.graphql.operationName = this.query.operationName || this.request.body.operationName
+  // Get GraphQL params from the request and POST body data
+  const { operationName, query, variables } = getGraphQLParams(ctx, body)
 
-  // Populate rootValue
-  Object.assign(this.graphql.rootValue, {
-    // uploaded files
-    files: this.request.files || {},
-
-    // userId from session
-    userId: this.session.userId,
-
-    // requestId
-    requestId: this.request.requestId
+  // Run the GraphQL query
+  const result = await runQuery(ctx, {
+    schema,
+    rootValue,
+    variables,
+    operationName,
+    query
   })
-
-  yield next
-}
-
-// Execute the GraphQL query
-GraphqlController.perform = function * perform () {
-  const { query, variables, operationName, schema, rootValue } = this.graphql
-  const result = yield graphql(schema, query, rootValue, variables, operationName)
 
   // Format any encountered errors
   if (result.errors) {
     result.errors = result.errors.map(formatError)
   }
 
-  // Report 200 success if a data key exists,
-  // Othewise 400 bad request if only errors exist
-  this.status = result.hasOwnProperty('data') ? 200 : 400
-  this.body = result
+  // Report 200:Success if a data key exists,
+  // Otherwise 400:BadRequest if only errors exist
+  ctx.status = result.hasOwnProperty('data') ? 200 : 400
+  ctx.body = result
+}
+
+/**
+ * Get the options that the middleware was configured
+ */
+async function getOptions (options, ctx) {
+  const optionsData = typeof options === 'function'
+    ? await options(ctx)
+    : options
+
+  invariant(
+    optionsData || typeof optionsData !== 'object',
+    'GraphQL middleware option function must return an options object.'
+  )
+
+  invariant(
+    optionsData.schema,
+    'GraphQL middleware options must contain a schema.'
+  )
+
+  return optionsData
+}
+
+/**
+ * Helper function to parse the body
+ */
+async function parseBody (ctx) {
+  // Skip requests without content type
+  const hasContentType = ctx.request.type !== undefined
+
+  // If koa already has body, use it
+  const hasEmptyBody = ctx.request.body === undefined
+
+  if (hasEmptyBody && hasContentType) {
+    switch (ctx.request.type) {
+      case 'application/graphql':
+        const body = await bodyParser.text(ctx, PARSE_OPTIONS)
+        ctx.request.body = { query: body }
+        break
+      case 'application/json':
+        ctx.request.body = await bodyParser.json(ctx, PARSE_OPTIONS)
+        break
+      case 'application/x-www-form-encoded':
+        ctx.request.body = await bodyParser.form(ctx, PARSE_OPTIONS)
+        break
+      default:
+        ctx.request.body = {}
+        break
+    }
+  }
+  return ctx.request.body
+}
+
+/**
+ * Get the GraphQL params from the request
+ */
+function getGraphQLParams (ctx, body = {}) {
+  // GraphQL Query String
+  const query = ctx.query.query || body.query
+  ctx.assert(query, 400, 'Must provide query string.')
+
+  // Parse the variables if needed
+  const variables = getVariables(ctx.query.variables || body.variables)
+
+  // Name of GraphQL operation to execute
+  const operationName = ctx.query.operationName || body.operationName
+
+  return {
+    operationName,
+    query,
+    variables
+  }
+
+  // Try parsing the variables JSON if there's any
+  function getVariables (variables) {
+    if (variables && typeof variables === 'string') {
+      try {
+        return JSON.parse(variables)
+      } catch (err) {
+        throw createError(400, 'Variables are invalid JSON.')
+      }
+    }
+  }
+}
+
+/**
+ * Run GraphQL query
+ */
+async function runQuery (ctx, { schema, rootValue, variables, operationName, query }) {
+  const source = new Source(query, 'GraphQL request')
+  const documentAST = parse(source)
+  const validationErrors = validate(schema, documentAST)
+  if (validationErrors.length) {
+    return { errors: validationErrors }
+  } else {
+    // Only query operations are allowed on GET requests
+    if (ctx.method === 'GET') {
+      // Determine if this GET request will perform a non-query
+      const operationAST = getOperationAST(documentAST, operationName)
+      if (operationAST && operationAST.operation !== 'query') {
+        ctx.set('Allow', 'POST')
+        ctx.throw(
+          405,
+          `Can only perform a ${operationAST.operation} operation from a POST request.`
+        )
+      }
+    }
+
+    // Perform the execution
+    return await execute(
+      schema,
+      documentAST,
+      rootValue,
+      variables,
+      operationName
+    )
+  }
 }
