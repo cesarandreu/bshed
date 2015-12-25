f0e5de85e4b43cfe44a06a994aea3136c0f569df
Cesar Andreu <cesarandreu@gmail.com>
2015-12-25 00:09:15 -0800

    Add graphiql middleware

---
+++ server/lib/graphiql.js
@@ -0,0 +1,101 @@
+/**
+ * Render GraphiQL for a nice debugging experience~
+ * SOURCE: https://github.com/graphql/express-graphql/blob/master/src/renderGraphiQL.js
+ */
+import { noopMiddleware } from './noop'
+
+const GRAPHIQL_VERSION = '0.4.4'
+const GRAPHQL_PATH = '/graphql'
+
+export default function graphiql ({ enabled }) {
+  return enabled
+    ? graphiqlMiddleware
+    : noopMiddleware
+}
+
+function graphiqlMiddleware (ctx) {
+  ctx.set('Content-Type', 'text/html')
+  ctx.body = `
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.css" rel="stylesheet" />
+  <script src="//cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js"></script>
+  <script src="//cdn.jsdelivr.net/react/0.14.3/react.min.js"></script>
+  <script src="//cdn.jsdelivr.net/react/0.14.3/react-dom.min.js"></script>
+  <script src="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.min.js"></script>
+</head>
+<body>
+  <script>
+    // Collect the URL parameters
+    var parameters = {};
+    window.location.search.substr(1).split('&').forEach(function (entry) {
+      var eq = entry.indexOf('=');
+      if (eq >= 0) {
+        parameters[decodeURIComponent(entry.slice(0, eq))] =
+          decodeURIComponent(entry.slice(eq + 1));
+      }
+    });
+    // Produce a Location query string from a parameter object.
+    function locationQuery(params) {
+      return '?' + Object.keys(params).map(function (key) {
+        return encodeURIComponent(key) + '=' +
+          encodeURIComponent(params[key]);
+      }).join('&');
+    }
+    // Derive a fetch URL from the current URL, sans the GraphQL parameters.
+    var graphqlParamNames = {
+      query: true,
+      variables: true,
+      operationName: true
+    };
+    var otherParams = {};
+    for (var k in parameters) {
+      if (parameters.hasOwnProperty(k) && graphqlParamNames[k] !== true) {
+        otherParams[k] = parameters[k];
+      }
+    }
+    var fetchURL = '${GRAPHQL_PATH}' + locationQuery(otherParams);
+    // Defines a GraphQL fetcher using the fetch API.
+    function graphQLFetcher(graphQLParams) {
+      return fetch(fetchURL, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(graphQLParams),
+        credentials: 'include',
+      }).then(function (response) {
+        return response.json()
+      });
+    }
+    // When the query and variables string is edited, update the URL bar so
+    // that it can be easily shared.
+    function onEditQuery(newQuery) {
+      parameters.query = newQuery;
+      updateURL();
+    }
+    function onEditVariables(newVariables) {
+      parameters.variables = newVariables;
+      updateURL();
+    }
+    function updateURL() {
+      history.replaceState(null, null, locationQuery(parameters));
+    }
+    // Render <GraphiQL /> into the body.
+    React.render(
+      React.createElement(GraphiQL, {
+        fetcher: graphQLFetcher,
+        onEditQuery: onEditQuery,
+        onEditVariables: onEditVariables,
+        query: '',
+        response: '',
+        variables: ''
+      }),
+      document.body
+    );
+  </script>
+</body>
+</html>`
+}
