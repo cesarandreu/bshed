56e266a907ed77a32d95d40850d212254abf91dc
Cesar Andreu <cesarandreu@gmail.com>
2015-12-26 06:20:04 -0800

    Clean up and refactor Button component

--- client/components/Button/Button.js
+++ client/components/Button/Button.js
@@ -1,131 +1,56 @@
 import cn from 'classnames'
 import styles from './Button.css'
 import React, { Component, PropTypes } from 'react'
+import { Icon } from 'components/Icon'
 
 export class Button extends Component {
-  constructor (props, context) {
-    super(props, context)
-    this.state = {
-      focused: false,
-      pressed: false
-    }
-    this._onBlur = this._onBlur.bind(this)
-    this._onFocus = this._onFocus.bind(this)
-    this._onKeyDown = this._onKeyDown.bind(this)
-    this._onKeyUp = this._onKeyUp.bind(this)
-    this._onMouseDown = this._onMouseDown.bind(this)
-    this._onMouseUp = this._onMouseUp.bind(this)
-    this._setButton = this._setButton.bind(this)
-  }
-
-  _onBlur () {
-    this.setState({
-      focused: false
-    })
-  }
-
-  _onFocus () {
-    this.setState({
-      focused: true
-    })
-  }
-
-  _onKeyDown (e) {
-    if ([' ', 'Enter'].includes(e.key)) {
-      this.setState({
-        pressed: true
-      })
-    }
-  }
-
-  _onKeyUp (e) {
-    if ([' ', 'Enter'].includes(e.key)) {
-      this.setState({
-        pressed: false
-      })
-      this._button.click()
-    }
-  }
-
-  _onMouseDown () {
-    this.setState({
-      pressed: true
-    })
-  }
-
-  _onMouseUp () {
-    this.setState({
-      pressed: false
-    })
-  }
-
-  _setButton (button) {
-    this._button = button
-  }
-
   getClassNames () {
-    const { pressed } = this.state
-    const { raised } = this.props
-    return cn(styles.button, styles[`elevation${this.getElevation()}`], {
-      [styles.pressed]: pressed,
-      [styles.raised]: raised
+    const { flat, primary, raised, secondary } = this.props
+    const plain = !primary && !secondary
+    return cn(styles.base, {
+      [styles.plainFlat]: flat && plain,
+      [styles.plainRaised]: raised && plain,
+      [styles.primaryFlat]: flat && primary,
+      [styles.primaryRaised]: raised && primary,
+      [styles.secondaryFlat]: flat && secondary,
+      [styles.secondaryRaised]: raised && secondary
     })
-  }
-
-  getElevation () {
-    const { focused, pressed } = this.state
-    const { disabled, raised } = this.props
-    if (disabled || !raised) {
-      return 0
-    } else if (pressed) {
-      return 4
-    } else if (focused) {
-      return 3
-    } else {
-      return 1
-    }
-  }
-
-  getTabIndex () {
-    const { disabled } = this.props
-    return disabled ? -1 : 0
   }
 
   render () {
-    const { pressed } = this.state
-    const { children, disabled, onClick, raised } = this.props
+    const { children, disabled, icon, onClick } = this.props
     return (
-      <div
-        aria-disabled={disabled}
-        aria-pressed={pressed}
+      <button
         className={this.getClassNames()}
         disabled={disabled}
-        onBlur={this._onBlur}
         onClick={onClick}
-        onFocus={this._onFocus}
-        onKeyDown={this._onKeyDown}
-        onKeyUp={this._onKeyUp}
-        onMouseDown={this._onMouseDown}
-        onMouseUp={this._onMouseUp}
-        raised={raised}
-        ref={this._setButton}
-        role='button'
-        tabIndex={this.getTabIndex()}
+        type='button'
       >
+        {icon && (
+          <Icon type={icon}/>
+        )}
         {children}
-      </div>
+      </button>
     )
   }
 }
 
 Button.defaultProps = {
   disabled: false,
-  raised: false
+  flat: false,
+  icon: '',
+  primary: false,
+  raised: false,
+  secondary: false
 }
 
 Button.propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
   disabled: PropTypes.bool,
+  flat: PropTypes.bool,
+  icon: PropTypes.string,
   onClick: PropTypes.func,
-  raised: PropTypes.bool
+  primary: PropTypes.bool,
+  raised: PropTypes.bool,
+  secondary: PropTypes.bool
 }
