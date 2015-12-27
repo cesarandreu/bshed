52eb63d63182389b138346938b6a3b637961f51b
Cesar Andreu <cesarandreu@gmail.com>
2015-12-26 23:01:22 -0800

    Stop using bshed alias

--- config/babel-relay-plugin.js
+++ config/babel-relay-plugin.js
@@ -2,7 +2,7 @@
  * Load graphql schema and export the babel relay plugin
  * Used to compile frontend code
  */
-import schema from 'bshed/data/schema.json'
+import schema from '../data/schema.json'
 import getBabelRelayPlugin from 'babel-relay-plugin'
 
 export default getBabelRelayPlugin(schema.data)
