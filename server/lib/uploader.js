4cc73541e3bfeb74abc3a401cefd2d78a92cb0c2
Cesar Andreu <cesarandreu@gmail.com>
2015-12-28 02:05:21 -0800

    Update uploader middleware to handle new behavior

--- server/lib/uploader.js
+++ server/lib/uploader.js
@@ -2,73 +2,86 @@
  * Uploader
  * @flow
  */
-import s3 from 'multer-s3'
+import {
+  ALLOWED_MIMETYPES,
+  MAXIMUM_IMAGE_COUNT,
+  MAXIMUM_IMAGE_SIZE
+} from 'bshed-constants'
+import debug from 'debug'
 import multer from 'multer'
+import s3 from 'multer-s3'
+import { times } from 'lodash'
 
-const ALLOWED_MIMETYPES = ['image/jpeg', 'image/png']
+const log = debug('server:uploader')
+const validFileFields = times(MAXIMUM_IMAGE_COUNT, n => `${n}`)
 
 function fileFilter (req, file, cb) {
-  const hasAllowedMimetype = ALLOWED_MIMETYPES.includes(file.mimetype)
-  const hasRequestId = !!req.requestId
+  const hasValidField = validFileFields.includes(file.fieldname)
   const hasUserId = !!req.userId
-
-  const isAllowed = hasAllowedMimetype && hasRequestId && hasUserId
-  cb(null, isAllowed)
+  const hasRequestId = !!req.requestId
+  const hasAllowedMimetype = ALLOWED_MIMETYPES.includes(file.mimetype)
+  cb(null, hasValidField && hasAllowedMimetype && hasRequestId && hasUserId)
 }
 
 function filename (req, file, cb) {
-  if (!req.requestId) {
-    cb(new Error('Must have requestId'))
-  } else {
-    cb(null, `${req.userId}/${req.requestId}/${file.fieldname}`)
-  }
+  const { fieldname } = file
+  const { userId, requestId } = req
+  cb(null, `${userId}/${requestId}/${fieldname}`)
 }
 
-export default function createUploader (config: Object): Function {
+export function createUploader (awsConfig: Object): Function {
   const uploader = multer({
     fileFilter,
     limit: {
-      fileSize: 2e6, // 2MB
-      fields: 10,
-      files: 5
+      fileSize: MAXIMUM_IMAGE_SIZE, // 3MB
+      fields: 2, // duration and title
+      files: MAXIMUM_IMAGE_COUNT
     },
     storage: s3({
       dirname: 'uploads/images',
       filename,
-      ...config
+      ...awsConfig
     })
   })
   .fields([{
-    name: 'image0',
-    maxCount: 1
+    // Bikeshed duration
+    maxCount: 1,
+    name: 'duration'
   }, {
-    name: 'image1',
-    maxCount: 1
-  }, {
-    name: 'image2',
-    maxCount: 1
-  }, {
-    name: 'image3',
-    maxCount: 1
-  }, {
-    name: 'image4',
-    maxCount: 1
-  }, {
-    name: 'image5',
-    maxCount: 1
-  }, {
-    name: 'image6',
-    maxCount: 1
-  }, {
-    name: 'image7',
-    maxCount: 1
-  }])
+    // Bikeshed title
+    maxCount: 1,
+    name: 'title'
+  },
+  // Bikes
+  ...times(MAXIMUM_IMAGE_COUNT, n => ({ maxCount: 1, name: `${n}` }))
+  ])
 
   return function performUpload (req, res) {
     return new Promise((resolve, reject) => {
       uploader(req, res, err =>
-        err ? reject(err) : resolve()
+        err ? reject(err) : resolve({ body: req.body, files: req.files })
       )
     })
+  }
+}
+
+// Upload all the files
+// @TODO: Use different uploaders depending on the mutation type
+export default function uploader (awsConfig: Object): Function {
+  const performUpload = createUploader(awsConfig)
+  return async function uploaderMiddleware (ctx, next) {
+    if (ctx.method === 'POST' && ctx.is('multipart/form-data')) {
+      log('performing upload')
+      const { body, files } = await performUpload(ctx.req, ctx.res)
+      ctx.request.body = body
+
+      // Pull out each file from the inner array
+      ctx.request.files = Object.entries(files)
+      .reduce((files, [name, [file]]) => {
+        files[name] = file
+        return files
+      }, {})
+    }
+    return next()
   }
 }
