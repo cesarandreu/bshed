a962329960e59103308af9ec1324010d69e5e98e
Cesar Andreu <cesarandreu@gmail.com>
2016-01-03 01:57:25 -0800

    Add PaperPage component, remove Page component

--- client/components/Page/Page.js
+++ client/components/Page/Page.js
@@ -1,14 +1,24 @@
+import cn from 'classnames'
+import { DetectEnvironment } from 'client/utils/DetectEnvironment'
+import { MOBILE_SCREEN_MEDIA } from 'components/styles/constants'
 import React, { PropTypes } from 'react'
 import styles from './Page.css'
 
-export function Page ({ children }) {
+function PaperPage ({ children, isDesktop }) {
   return (
-    <div className={styles.page}>
+    <div
+      className={cn(styles.paperPage, {
+        [styles.paperPageShadow]: isDesktop
+      })}
+    >
       {children}
     </div>
   )
 }
 
-Page.propTypes = {
-  children: PropTypes.node.isRequired
+PaperPage.propTypes = {
+  children: PropTypes.node.isRequired,
+  isDesktop: PropTypes.bool.isRequired
 }
+
+export const PaperPageContainer = DetectEnvironment(MOBILE_SCREEN_MEDIA)(PaperPage)
