8a26ec026b69ceb02b869509da243bd4cc521366
Cesar Andreu <cesarandreu@gmail.com>
2016-01-03 02:06:52 -0800

    Fix Terms page

--- client/pages/Terms/Terms.js
+++ client/pages/Terms/Terms.js
@@ -2,19 +2,22 @@
  * This page shows our terms of use
  * @TODO: Get terms of use
  */
-import { Layout, LayoutContent, LayoutToolbar } from 'components/Layout'
+import { LayoutToolbar, PageLayoutContainer } from 'components/Layout'
 import React from 'react'
 import styles from './Terms.css'
 
 export default function Terms () {
   return (
-    <Layout>
-      <LayoutToolbar title='Bikeshed'/>
-      <LayoutContent>
-        <div className={styles.terms}>
-          Terms~
-        </div>
-      </LayoutContent>
-    </Layout>
+    <PageLayoutContainer
+      toolbar={
+        <LayoutToolbar
+          title='Terms of service'
+        />
+      }
+    >
+      <div className={styles.terms}>
+        Terms~
+      </div>
+    </PageLayoutContainer>
   )
 }
