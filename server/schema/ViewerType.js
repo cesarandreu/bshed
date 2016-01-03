0162a73c5eac8c367b2090924d6063d0f7fbdedf
Cesar Andreu <cesarandreu@gmail.com>
2016-01-03 02:16:52 -0800

    Add ViewerType

---
+++ server/schema/ViewerType.js
@@ -0,0 +1,31 @@
+import {
+  // GraphQLBoolean,
+  GraphQLObjectType
+  // GraphQLString
+} from 'graphql'
+import {
+  connectionArgs
+  // globalIdField
+} from 'graphql-relay'
+
+export default function getViewerType ({ types }) {
+  types.ViewerType = new GraphQLObjectType({
+    name: 'Viewer',
+    description: 'Current user',
+    fields () {
+      return {
+        user: {
+          type: types.UserType,
+          resolve (_, args, { rootValue: { models, userId } }) {
+            return new models.User({ id: userId }).fetch()
+          }
+        },
+
+        bikesheds: {
+          args: connectionArgs,
+          type: types.BikeshedConnectionType
+        }
+      }
+    }
+  })
+}
