1e87b1cb4e34123bdf47ab8e49c454f787e42de1
Cesar Andreu <cesarandreu@gmail.com>
2016-01-03 02:04:15 -0800

    Update PreloadImage to position itself manually and to use the improved animation

--- client/components/PreloadImage/PreloadImage.js
+++ client/components/PreloadImage/PreloadImage.js
@@ -1,22 +1,48 @@
+import { easeOut } from 'bezier-easing'
 import cn from 'classnames'
+import { createAnimation } from 'client/utils/createAnimation'
 import React, { Component, PropTypes } from 'react'
 import styles from './PreloadImage.css'
+
+const IMAGE_LOAD_DURATION = 1.5e3
 
 export class PreloadImage extends Component {
   constructor (props, context) {
     super(props, context)
     this.state = {
-      hasLoaded: false,
-      ratio: 1
+      animating: false,
+      grayscale: 1,
+      height: 0,
+      opacity: 0,
+      saturate: 0.20,
+      width: 0
     }
     this._getImage = this._getImage.bind(this)
     this._onLoad = this._onLoad.bind(this)
+    this.animation = createAnimation(
+      this.paint.bind(this),
+      IMAGE_LOAD_DURATION,
+      () => this.setState({ animating: true }),
+      () => this.setState({ animating: false })
+    )
   }
 
-  _onLoad () {
+  componentWillUnmount () {
+    this.animation.stop()
+  }
+
+  // There's 3 steps in the image load animation
+  // opacity lasts from 1 to 2 with percentage 0 to 1
+  // grayscale lasts from 1 to 2.5 with percentage 1 to 0
+  // saturate lasts from 1 to 3 with percentage 0.20 to 1
+  paint (timeFraction) {
+    const opacityFraction = Math.min(timeFraction, 0.5) / 0.5
+    const grayscaleFraction = Math.min(timeFraction, 0.75) / 0.75
+
     this.setState({
-      hasLoaded: true,
-      ratio: (this.image.naturalWidth / this.image.naturalHeight) || 1
+      opacity: easeOut.get(opacityFraction),
+      grayscale: 1 - easeOut.get(grayscaleFraction),
+      saturate: 0.2 + (easeOut.get(timeFraction) * 0.8)
     })
   }
 
@@ -24,27 +50,72 @@ export class PreloadImage extends Component {
     this.image = image
   }
 
+  _onLoad () {
+    const image = this.image
+    this.setState({
+      height: image.naturalHeight,
+      width: image.naturalWidth
+    })
+    this.animation.start()
+  }
+
+  // Here we cast all the size and position numbers to integers
+  // Handling of non-integer <number> values in css is inconsistent across browsers
+  calculateImageStyles () {
+    const { grayscale, height, opacity, saturate, width } = this.state
+    const { size } = this.props
+
+    // First take the smaller of the two dimensions
+    // That will be the boundary
+    const smallestDimension = Math.min(height, width)
+
+    // Get the percentage we'd need to scale the image to fit in the box
+    // We cap it at 1 so the smaller dimension can't grow past the size
+    // If the smallest dimension is 0px it'll give us NaN, in which we case we use 1
+    const percent = Math.min((size / smallestDimension), 1) || 1
+
+    // Scale the height and width
+    const h = percent * height | 0
+    const w = percent * width | 0
+
+    // Get the half size to help calculating the position
+    // In css it would be `calc(50% - ${dimension / 2}px)`
+    const halfSize = size / 2 | 0
+
+    // Animated filter appearance
+    const filter = `opacity(${opacity}) grayscale(${grayscale}) saturate(${saturate})`
+
+    return {
+      filter: filter,
+      height: h,
+      left: halfSize - (w / 2) | 0,
+      top: halfSize - (h / 2) | 0,
+      WebkitFilter: filter,
+      width: w
+    }
+  }
+
   render () {
     const { size, src } = this.props
-    const { hasLoaded, ratio } = this.state
-
-    const imageClassNames = cn(styles.image, styles.animated, {
-      [styles.loaded]: hasLoaded,
-      [styles.tallImage]: ratio <= 1,
-      [styles.wideImage]: ratio >= 1
+    const { animating } = this.state
+    const imageClassNames = cn(styles.image, {
+      [styles.animating]: animating
     })
 
     return (
-      <div className={styles.wrapper}>
+      <div
+        className={styles.wrapper}
+        style={{
+          height: size,
+          width: size
+        }}
+      >
         <img
           className={imageClassNames}
           onLoad={this._onLoad}
           ref={this._getImage}
           src={src}
-          style={{
-            top: `calc(50% - ${size / 2}px)`,
-            left: `calc(50% - ${size / 2}px)`
-          }}
+          style={this.calculateImageStyles()}
         />
       </div>
     )
