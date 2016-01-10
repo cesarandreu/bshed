7979b6b5990ef3b45fdced1bee3b8b70ef9b941f
Cesar Andreu <cesarandreu@gmail.com>
2016-01-09 19:41:04 -0800

    Move middleware to its own folder

--- server/middleware/exposeError.js
+++ server/middleware/exposeError.js
@@ -1,9 +1,9 @@
-import debug from 'debug'
-const log = debug('server:error')
-
 /**
  * Middleware to expose errors
  */
+import debug from 'debug'
+const log = debug('server:error')
+
 export function exposeError () {
   return function exposeErrorMiddleware (ctx, next) {
     return next().catch(err => {
