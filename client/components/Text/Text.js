cb82d391db3c3cd3e0802cd5aeae3ff2ced5c5f6
Cesar Andreu <cesarandreu@gmail.com>
2016-03-02 20:43:57 -0800

    Update Text components

--- client/components/Text/Text.js
+++ client/components/Text/Text.js
@@ -1,51 +1,133 @@
+/**
+ * These are all the Text components the app uses
+ * The elements used by some of these were inspired by PolymerElements/paper-styles
+ * h1 => display2
+ * h2 => display1
+ * h3 => headline
+ * h4 => subhead
+ * They also have h5, h6 => body2, but I don't know if that's useful
+ */
 import cn from 'classnames'
-import React from 'react'
+import React, { PropTypes } from 'react'
 import styles from './Text.css'
 
-export function Caption ({ children }) {
-  return (
-    <div className={styles.caption}>
-      {children}
-    </div>
-  )
+export function textComponentFactory ({ defaultColorClassName, name, type }) {
+  const textClassName = styles[name]
+  function TextComponent ({
+    className,
+    custom,
+    dark,
+    disabled,
+    hint,
+    light,
+    primary,
+    secondary,
+    ...props
+  }) {
+    const noTheme = !dark && !light
+    const noColor = !disabled && !hint && !primary && !secondary
+
+    return React.createElement(type, {
+      className: cn(textClassName, className, {
+        [defaultColorClassName]: !custom && noTheme && noColor,
+        [styles.DarkDisabled]: dark && disabled,
+        [styles.DarkHint]: dark && hint,
+        [styles.DarkPrimary]: dark && primary,
+        [styles.DarkSecondary]: dark && secondary,
+        [styles.LightDisabled]: light && disabled,
+        [styles.LightHint]: light && hint,
+        [styles.LightPrimary]: light && primary,
+        [styles.LightSecondary]: light && secondary
+      }),
+      ...props
+    })
+  }
+  Object.assign(TextComponent, {
+    displayName: name,
+    name: name,
+    propTypes: {
+      children: PropTypes.node.isRequired,
+      className: PropTypes.string,
+      custom: PropTypes.bool,
+      dark: PropTypes.bool,
+      disabled: PropTypes.bool,
+      hint: PropTypes.bool,
+      light: PropTypes.bool,
+      primary: PropTypes.bool,
+      secondary: PropTypes.bool
+    }
+  })
+  return TextComponent
 }
 
-export function Headline ({ children }) {
-  return (
-    <h3 className={styles.headline}>
-      {children}
-    </h3>
-  )
-}
+// Base text components
+export const Display4 = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Display4',
+  type: 'span'
+})
 
-export function Hint ({ children, className }) {
-  return (
-    <div className={cn(styles.hint, className)}>
-      {children}
-    </div>
-  )
-}
+export const Display3 = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Display3',
+  type: 'span'
+})
 
-export function PrimaryText ({ children, className }) {
-  return (
-    <div className={cn(styles.primary, className)}>
-      {children}
-    </div>
-  )
-}
+export const Display2 = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Display2',
+  type: 'h1'
+})
 
-export function SecondaryText ({ children, className }) {
-  return (
-    <div className={cn(styles.secondary, className)}>
-      {children}
-    </div>
-  )
-}
+export const Display1 = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Display1',
+  type: 'h2'
+})
 
-export function Title ({ children, className }) {
-  return (
-    <div className={cn(styles.title, className)}>
-      {children}
-    </div>
-  )
+export const Headline = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Headline',
+  type: 'h3'
+})
+
+export const Title = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Title',
+  type: 'span'
+})
+
+export const Subhead = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Subhead',
+  type: 'h4'
+})
+
+export const Body2 = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Body2',
+  type: 'span'
+})
+
+export const Body1 = textComponentFactory({
+  defaultColorClassName: styles.DarkSecondary,
+  name: 'Body1',
+  type: 'span'
+})
+
+export const Caption = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Caption',
+  type: 'span'
+})
+
+export const ButtonText = textComponentFactory({
+  defaultColorClassName: styles.DarkPrimary,
+  name: 'Button',
+  type: 'span'
+})
+
+// Alias and wrapper text components
+export function Hint (props) {
+  return <Caption dark hint {...props}/>
 }
--- client/components/Text/index.js
+++ client/components/Text/index.js
@@ -1,8 +1,17 @@
 export {
-  Caption,
+  // Base
+  Display4,
+  Display3,
+  Display2,
+  Display1,
   Headline,
-  Hint,
-  PrimaryText,
-  SecondaryText,
-  Title
+  Title,
+  Subhead,
+  Body2,
+  Body1,
+  Caption,
+  ButtonText,
+
+  // Aliases and wrappers
+  Hint
 } from './Text'
