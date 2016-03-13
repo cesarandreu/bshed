cfdd69c7fa0747bbe4656da2761df3b2139f1095
Cesar Andreu <cesarandreu@gmail.com>
2016-03-13 11:52:38 -0700

    Update knew config

--- knexfile.js
+++ knexfile.js
@@ -1,33 +1,10 @@
+const config = require('server/config')
+const bshedConfig = config.get('bshed')
+
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      database: 'bshed_development',
-      host: '127.0.0.1',
-      port: '5432',
-      user: 'root'
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-  test: {
-    client: 'pg',
-    connection: {
-      database: 'bshed_test',
-      host: '127.0.0.1',
-      port: '5432',
-      user: 'root'
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'pg',
+  connection: bshedConfig.drivers.pg,
+  migration: {
+    tableName: 'knex_migrations'
   }
 }
