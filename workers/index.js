1d44d1b7fb384f9edf694ebb8dbb05ead5dec4a8
Cesar Andreu <cesarandreu@gmail.com>
2016-04-15 14:34:40 -0700

    Remove old workers

--- workers/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Workers
- */
-export * as ImageUpdatesWorker from './image-updates'
-export * as ProcessImagesWorker from './process-images'
-export * as VoteUpdatesWorker from './vote-updates'
